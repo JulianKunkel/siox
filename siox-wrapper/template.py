c95fd5f36ab825d3308201ab2507f4319a9afb04
Roman Michel <rmi6chr@gmail.com>
2012-06-26 14:09:58 +0200

    Minor template changes

--- siox-wrapper/template.py
+++ siox-wrapper/template.py
@@ -1,16 +1,16 @@
 template = {
-'register_node': { # angepasst
+'register_node': {
 	'variables': 'HWID SWID OntName',
 	'global': '''siox_unid global_unid;
                  char global_pid;''',
-    'init': '''sprintf( global_pid, "%%d", getpid() );
-               global_unid = siox_register_node("%(HWID)s", "%(SWID)s", global_pid);
+    'init': '''sprintf( &global_pid, "%%d", getpid() );
+               global_unid = siox_register_node("%(HWID)s", "%(SWID)s", &global_pid);
                siox_set_ontology("%(OntName)s");''',
 	'before': '',
 	'after': '',
 	'final': 'siox_unregister_node(global_unid);'
 },
-'register_descriptor': { # angepasst
+'register_descriptor': {
 	'variables': 'Typ1 Typ2 DmidName',
 	'global': '''siox_dmid %(DmidName)s;''',
 	'init': '''%(DmidName)s = siox_register_descriptor_map( global_unid, %(Typ1)s, %(Typ2)s );''',
@@ -18,7 +18,7 @@ template = {
 	'after': '',
 	'final': ''
 },
-'register_edge': { # angepasst
+'register_edge': { 
 	'variables': 'Edge',
 	'global': '''''',
 	'init': '''siox_register_edge( global_unid, "%(Edge)s" );''',
@@ -26,7 +26,7 @@ template = {
 	'after': '',
 	'final': ''
 },
-'register_datatype': { # angepasst
+'register_datatype': { 
 	'variables': 'Name MinStorage RetName',
 	'global': '''siox_dtid %(RetName)s;''',
 	'init': '''%(RetName)s = siox_register_datatype( "%(Name)s", "%(MinStorage)s" );''',
@@ -34,7 +34,7 @@ template = {
 	'after': '',
 	'final': ''
 },
-'register_attribute': { # angepasst
+'register_attribute': {
 	'variables': 'Key ValueType Value',
 	'global': '''''',
 	'init': '''siox_register_attribute( global_unid, "%(Key)s", "%(ValueType)s", "%(Value)s" );''',
@@ -42,7 +42,7 @@ template = {
 	'after': '',
 	'final': ''
 },
-'register_metric': { # angepasst
+'register_metric': {
 	'variables': 'Name Description UnitType StorageType ScopeType',
 	'global': '''''',
 	'init': '''siox_register_metric( "%(Name)s", "%(Description)s", "%(UnitType)s", "%(StorageType)s", "%(ScopeType)s" );''',
@@ -50,7 +50,7 @@ template = {
 	'after': '',
 	'final': ''
 },
-'activity': { # angepasst
+'activity': { 
 	'variables': 'Name Description',
 	'global': '''siox_aid %(Name)s;''',
 	'init': '''''',
@@ -58,7 +58,7 @@ template = {
 	'after': 'siox_stop_activity( "%(Name)s" );',
 	'final': ''
 },
-'activity_start': { # angepasst
+'activity_start': {
 	'variables': 'Name Description',
 	'global': '''siox_aid %(Name)s;''',
 	'init': '''''',
@@ -66,7 +66,7 @@ template = {
 	'after': '',
 	'final': ''
 },
-'activity_stop': { # angepasst
+'activity_stop': { 
 	'variables': 'Name',
 	'global': '''''',
 	'init': '''''',
@@ -74,7 +74,7 @@ template = {
 	'after': 'siox_stop_activity( "%(Name)s" );',
 	'final': ''
 },
-'activity_end': { # angepasst
+'activity_end': {
 	'variables': 'Name',
 	'global': '''''',
 	'init': '''''',
@@ -82,15 +82,15 @@ template = {
 	'after': 'siox_end_activity( "%(Name)s" );',
 	'final': ''
 },
-'activity_report': { # angepasst
+'activity_report': {
 	'variables': 'ActivityId DescriptorId DescriptorName MapId ValueType Value Details',
 	'global': '''''',
 	'init': '''''',
-    'before': '''siox_report_activity(%(ActivityId)s, %(DescriptorId)s, %(DescriptorName)s, %(MapId)s, %(ValueType)s, %(Value)s, %(Details)s);''',
-	'after': '',
+    'before': '''''',
+	'after': 'siox_report_activity(%(ActivityId)s, %(DescriptorId)s, %(DescriptorName)s, %(MapId)s, %(ValueType)s, %(Value)s, %(Details)s);',
 	'final': ''
 },
-'create_descriptor': { # angepasst
+'create_descriptor': {
 	'variables': 'Type DescriptorName',
 	'global': '''''',
 	'init': '''siox_create_descriptor( global_unid, "%(Type)s", "%(DescriptorName)s");''',
@@ -98,7 +98,7 @@ template = {
 	'after': '',
 	'final': 'siox_release_descriptor( global_unid, "%(Type)s", "%(DescriptorName)s");'
 },
-'send_descriptor': { # angepasst
+'send_descriptor': {
 	'variables': 'TargetSWID Type DescriptorName',
 	'global': '''''',
 	'init': '''''',
@@ -106,7 +106,7 @@ template = {
 	'after': '',
 	'final': ''
 },
-'map_descriptor': { # angepasst
+'map_descriptor': {
 	'variables': 'Descriptor From To',
 	'global': '''''',
 	'init': '''''',
@@ -114,7 +114,7 @@ template = {
 	'after': 'siox_map_descriptor( global_unid, %(Descriptor)s, %(From)s, %(To)s);',
 	'final': ''
 },
-'receive_descriptor': { # angepasst
+'receive_descriptor': {
 	'variables': 'Type Name',
 	'global': '''''',
 	'init': '''''',
@@ -139,6 +139,10 @@ template = {
 	'final': ''
 }
 }
+
+ForEachBefore = ""
+
+ForEachAfter = ""
 
 throwaway = ["((^\s*)|(\s+))extern\s+.*\("]
 
