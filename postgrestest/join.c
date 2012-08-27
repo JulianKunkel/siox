e1b8133dbe788ae0b7c591c0cb655f273c7f80a3
Roman Michel <rmi6chr@gmail.com>
2012-08-27 12:55:01 +0200

    Add a postgres test file

---
+++ postgrestest/join.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <libpq-fe.h>
+#include <glib.h>
+
+int main( int argc, char *argv[] ) {
+    // general storage needs
+    GArray *results;
+    GString *tmp = g_string_new("");
+
+    // libpq
+    PGconn *connection = NULL;
+    PGresult *result;
+
+    // connection-settings
+    const char *keywords[] = {"dbname", "hostaddr", "user", "password", "port", NULL};
+    const char *values[] = {"Transaktionssystem-1", "136.172.14.14", "postgres", "postgres", "48142", NULL};
+    
+    // open a connection to the database
+    connection = PQconnectdbParams(keywords, values, 0);
+    
+    // check if the connection has been successfully established
+    if (PQstatus(connection) != CONNECTION_OK) {
+            // ERROR: print the error message
+            fprintf(stderr, "Connection to database failed: %s", PQerrorMessage(connection));
+    }
+    else {
+        g_string_append(tmp, "SELECT * FROM jtest1;");
+        // query the database
+        result = PQexec(connection, tmp->str);
+
+        if (PQresultStatus(result) != PGRES_TUPLES_OK)
+        {
+            // ERROR: print the error message
+            fprintf(stderr, "Command failed: %s", PQresultErrorMessage(result));
+            // empty the result to prevent leakage
+            PQclear(result);
+        }
+        else {
+            int i = 0;
+            int j = 0;
+
+            printf("jtest1:\n");
+            // go through the table names and print them
+            for (i = 0; i < PQnfields(result); i++) {
+                printf("%-15s", PQfname(result, i));
+            }
+            printf("\n\n");
+
+            // go through all result and save them into a two-dimensional GArray
+            for (i = 0; i < PQntuples(result); i++) {
+                for (j = 0; j < PQnfields(result); j++) {
+                    // read the result and print it
+                    printf("%-15s", PQgetvalue(result, i, j));
+                }
+                printf("\n");
+            }
+
+            // empty the result to prevent leakage
+            PQclear(result);
+        }
+
+        g_string_append(tmp, "SELECT * FROM jtest2;");
+        // query the database
+        result = PQexec(connection, tmp->str);
+
+        if (PQresultStatus(result) != PGRES_TUPLES_OK)
+        {
+            // ERROR: print the error message
+            fprintf(stderr, "Command failed: %s", PQresultErrorMessage(result));
+            // empty the result to prevent leakage
+            PQclear(result);
+        }
+        else {
+            int i = 0;
+            int j = 0;
+
+            printf("\njtest2:\n");
+            // go through the table names and print them
+            for (i = 0; i < PQnfields(result); i++) {
+                printf("%-15s", PQfname(result, i));
+            }
+            printf("\n\n");
+
+            // go through all result and save them into a two-dimensional GArray
+            for (i = 0; i < PQntuples(result); i++) {
+                for (j = 0; j < PQnfields(result); j++) {
+                    // read the result and print it
+                    printf("%-15s", PQgetvalue(result, i, j));
+                }
+                printf("\n");
+            }
+
+            // empty the result to prevent leakage
+            PQclear(result);
+        }
+
+        g_string_append(tmp, "SELECT jtest1.id, jtest1.name, jtest2.alter FROM jtest1, jtest2 WHERE jtest1.id = jtest2.id;");
+        // query the database
+        result = PQexec(connection, tmp->str);
+
+        if (PQresultStatus(result) != PGRES_TUPLES_OK)
+        {
+            // ERROR: print the error message
+            fprintf(stderr, "Command failed: %s", PQresultErrorMessage(result));
+            // empty the result to prevent leakage
+            PQclear(result);
+        }
+        else {
+            int i = 0;
+            int j = 0;
+
+            printf("\nJoin aus jtest1 und jtest2:\n");
+            // go through the table names and print them
+            for (i = 0; i < PQnfields(result); i++) {
+                printf("%-15s", PQfname(result, i));
+            }
+            printf("\n\n");
+
+            // go through all result and save them into a two-dimensional GArray
+            for (i = 0; i < PQntuples(result); i++) {
+                for (j = 0; j < PQnfields(result); j++) {
+                    // read the result and print it
+                    printf("%-15s", PQgetvalue(result, i, j));
+                }
+                printf("\n");
+            }
+
+            // empty the result to prevent leakage
+            PQclear(result);
+        }        
+    }
+
+}
