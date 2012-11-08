5c80cd58790552c9a92e3ecd099e29e3591c1f1f
Alvaro Aguilera <alvaro.aguilera@tu-dresden.de>
2012-11-08 12:31:38 +0100

    Translation of some strings.

--- low-level-C-interface/siox-ll.c
+++ low-level-C-interface/siox-ll.c
@@ -1,136 +1,9 @@
 /**
- * @file    siox-ll.c
- *          Implementation des SIOX-Low-Level-Interfaces.
+ * @file siox-ll.c SIOX low-level interface source code.
  *
- * @authors Michaela Zimmer, Julian Kunkel & Marc Wiedemann
- * @date    2012
- *          GNU Public License.
- */
-
-/**
- * @mainpage Scalable I/O for Extreme Performance (SIOX)
- *
- * @section intro Introduction
- * 
- * Applications running on HPC systems use a file system to do their I/O. This 
- * mostly consists of the initial read of the input data, the periodical storage 
- * of checkpointing information to restore the execution state from in case of 
- * unexpected program termination, as well as of the eventual writing of the 
- * application's actual output data.
- * 
- * In order for the I/O operations not to become the scalability bottleneck of 
- * HPC applications, the file system and I/O infrastructure must keep pace with 
- * the increasing performance and number of computing cores present on HPC 
- * systems. In this context, a global optimization of the file system turns out 
- * to be very difficult or impossible. In part due to the disparate nature of 
- * the requirements and expectations of different user groups, and in part 
- * because currently there is no way to identify abnormal I/O behavior and trace 
- * it back to its source.
- * 
- * SIOX' main goal is to gain an overview of all the I/O activity taking place 
- * on a HPC system, and to use this information to optimize it. Initially, the 
- * project's scope spans the development of standardized interfaces to collect, 
- * reduce, and store performance data from all relevant layers. This information 
- * will then be analyzed and correlated with previously observed access patterns 
- * in order to gain an understanding of the characteristics and causal 
- * relationships of the system.
- * 
- * This knowledge will be the starting point for subsequent performance 
- * optimizations aimed at specific users and applications, carried out through 
- * e.g. the automatic tuning of Open MPI or file system parameters. Such 
- * use-profiles are going to be continuously created and not only helpful for 
- * optimization, but also when diagnosting acute performance problems, or when 
- * planning new aquisitions. In the course of the project, an holistic approach 
- * for I/O analysis should be conceived, implemented and applied. While SIOX' 
- * applicability is oriented towards HPC environments, it shouldn't be 
- * constricted to them. In this way, the integrated analysis of applications, 
- * file systems, and infrastructure could also be used for the future 
- * optimization of other scenarios e.g. the design of file system caches for 
- * mail servers. 
- * 
- * The following sections describe the current state of the project.
- * 
- * @section architecture SIOX Architecture and Workflow
- * 
- * While the definite architecture of the SIOX system is still subject of 
- * debate, some of its essential components are already well defined. Among them
- * are the SIOX daemons, the knowledge base and the central data warehouse. The
- * workflow between the different components is illustrated in the figure below.
- * 
- * @subsection client SIOX Client
- * 
- * The SIOX client serves as the interface between instrumented software 
- * (libraries and applications) and remote SIOX servers. It is implemented 
- * as a daemon whose job includes the correlation of the local I/O activities, 
- * reduction of redundancy, aggregation of information and the transmission of 
- * relevant performance and trace data to the SIOX servers. 
- * 
- * @subsection servers SIOX Servers
- *
- * SIOX' server functionality is accomplished by a group of separate server 
- * processes fulfilling different roles. 
- *
- * @subsubsection transaction Transaction System
- * 
- * The transaction system is responsible for the continuous collection and 
- * correlation across system boundaries of the flow of trace information sent by 
- * the distributed clients. The result produced by the transaction system is 
- * the causal chain of I/O events produced by an I/O activity at the application
- * layer, as well as a detailed performance evaluation of the relevant links in
- * order to reveal potential I/O bottlenecks. This information is then sent to 
- * the data warehouse server for storage.
- * 
- * @subsubsection datawarehouse Data Warehouse
- * 
- * The data warehouse server manages a persistent archive of historical I/O
- * information and the corresponding observed performance data. It decides 
- * whether the informaiton is worth keeping or not, and if so, it stores it in a
- * database for future consultation. The data warehouse is used periodically by 
- * the knowledge base and the transactions server.
- * 
- * @subsubsection knowledgebase Knowledge Base
- * 
- * The goal of the knowledge base is to capitalize on the historical data 
- * accumulated on the data warehouse to not only hint the user about possible 
- * I/O bottlenecks in his application, but also to autonomously optimize to I/O 
- * subsystem when a well-known I/O pattern has occurred.
- * 
- * @image html architecture.png "SIOX Workflow"
- * 
- * @section intrumentation Sofware Instrumentation
- * 
- * The are two ways to make an application profit from SIOX. The easiest one is 
- * to simply compile the application against a SIOX-enabled library stack. The 
- * other one is to manually instrument the application using the SIOX API 
- * designed for that purpose. Before presenting some examples of manual source
- * code instrumentation there are some fundamental concepts you should 
- * familiarize with.
- * 
- * @subsection nodes SIOX Nodes
- * 
- * A SIOX node is a logical functional unit consisting of hardware components 
- * and a software layer. It represent one of the conceptual links in the 
- * corresponding causal chain of I/O activities. A SIOX node is identified by 
- * the hardware it resides on (e.g. hostname), the software layer it originates
- * from (e.g. application name or library name) and the application's instance
- * (e.g. process or thread id). This identification scheme is very flexible 
- * though and allows the combination of different data to form the different 
- * Id's as long as their concatenation can be used as a unique key. SIOX indexes 
- * all nodes using an unique numeric key. Instrumented applications must 
- * register their SIOX nodes during startup as the first step.
- * 
- * @subsection activities SIOX Activities
- * 
- * A SIOX activity represents an I/O action taking place on a SIOX node. 
- * Activities can be nested and are created by wrapping one or more of the 
- * function calls that trigger I/O. They define the granularity at which the 
- * SIOX system will evalute the I/O events on that particular node.
- * 
- * @subsection activities SIOX Descriptors
- * 
- * 
- * @subsection activities SIOX Metrics
- * 
+ * @date 11/2012
+ * @copyright GNU Public License.
+ * @authors Michaela Zimmer, Julian Kunkel, Marc Wiedemann & Alvaro Aguilera
  */ 
 
 #include <stdio.h>
@@ -143,16 +16,21 @@
 #include "siox-ll.h"
 #include "../ontology/ontology.h"
 
-/** Die Default-Ontologie, falls keine andere per siox_set_ontology() gesetzt worden sein sollte. */
+/** 
+ * Default ontology in case no other ontology has been set using 
+ * siox_set_ontology(). */
 #define DEFAULT_ONTOLOGY "testontology"
 
-/** Haben wir initialise_ontology() bereits durchlaufen? */
+/** 
+ * Whether or not the ontology has been already initialized with 
+ * initialise_ontology().
+ */
 static bool initialised_ontology = false;
 
-/** Die zu nutzende Ontologie. Kann vor dem ersten Aufruf von initialise_ontology() per siox_set_ontology()
- *  gesetzt werden; ansonsten wird DEFAULT_ONTOLOGY verwendet. */
+/** 
+ * The ontology to use. Its value is set by initialize_ontology() using the 
+ * siox_set_ontology() call. Otherwise DEFAULT_ONTOLOGY will be used. */
 static char sOntology[80] = "";
-
 
 
 /** The next unassigned UNID. */
@@ -191,12 +69,10 @@ struct siox_rcid_t {
 
 
 /**
- * Initialisiere die Ontologie-Bibliothek.
- * Wird nur beim ersten Zugriff auf aus der Ontologie zu lesende Daten (etwa die siox_register_...-Funktionen)
- * aufgerufen.
- */
+ * Initialization of the ontology library.
+ * This function is called only once during the first read access of the 
+ * functions siox_register_*.  */
 static void initialise_ontology();
-
 
 
 siox_unid
