31cfc0ab411c962b8b5acd8e56b1dc63639a628f
Johann Weging <8weging@informatik.uni-hamburg.de>
2012-11-12 12:24:36 +0100

    Adapted the siox-wrapper template to the new siox-ll interface.

--- siox-wrapper/template.py
+++ siox-wrapper/template.py
@@ -1,10 +1,10 @@
 template = {
-# interface_name 
+# register_node
 # 
 # Registers (and ungeristers) a new node with SIOX and sets the ontology. 
 #
-# SWID: The name for this node
-'interface_name': {
+# SWID: The name (software id) for this node
+'register_node': {
 	'variables': 'SWID',
 	'global': '''siox_unid global_unid;
                  char global_pid;
@@ -14,7 +14,8 @@ template = {
 			   gethostname(hostname, 1023);
                sprintf( &global_pid, "%%d", getpid() );
                global_unid = siox_register_node(hostname, %(SWID)s, &global_pid);
-               siox_set_ontology("OntologyName");''',
+               siox_set_ontology("OntologyName");
+               GHashTable *aidHashTable = g_hash_table_new(g_str_hash, g_str_equal);''',
 	'before': '',
 	'after': '',
 	'cleanup': '',
@@ -36,16 +37,16 @@ template = {
 	'cleanup': '',
 	'final': ''
 },
-# attribute_register
+# node_attribute
 #
 # Reports other important attributes to SIOX
 #
 # DTID: Datatype-ID of the attribute
 # Value: Pointer to the real value of the attribute
-'attribute_register': {
+'node_attribute': {
 	'variables': 'DTID Value',
 	'global': '''''',
-	'init': '''siox_register_attribute( global_unid,  %(DTID)s, %(Value)s);''',
+	'init': '''siox_node_attribute( global_unid,  %(DTID)s, %(Value)s);''',
     'before': '''''',
 	'after': '',
 	'cleanup': '',
@@ -57,14 +58,13 @@ template = {
 #
 # RetName: Name of the variable to store the mid
 # Name: Name of the metric
-# Description: Short description of the metric
 # UnitType: SIOX unit-type for the metric
 # StorageType: SIOX storage-type for the metric
 # ScopeType: SIOX scope-type for the metric
 'metric_register': {
-	'variables': 'RetName Name Description UnitType StorageType ScopeType',
+	'variables': 'RetName Name UnitType StorageType ScopeType',
 	'global': '''siox_mid %(RetName)s;''',
-	'init': '''%(RetName)s = siox_register_metric( %(Name)s, %(Description)s, %(UnitType)s, %(StorageType)s, %(ScopeType)s );''',
+	'init': '''%(RetName)s = siox_register_metric( %(Name)s, %(UnitType)s, %(StorageType)s, %(ScopeType)s );''',
     'before': '''''',
 	'after': '',
 	'cleanup': '',
@@ -76,115 +76,147 @@ template = {
 # Still needs to be reported!
 #
 # AID: Name of the varibale to store the aid
+# TimeStart: Start time to be reported
+# TimeStop: Stop time to be reported
+# TimeEnd: End time to be reported
 # Description: Short description of the activity
 'activity': {
-	'variables': 'AID Description',
+	'variables': 'AID TimeStart TimeStop TimeEnd Description',
 	'global': '''''',
 	'init': '''''',
-    'before': '''siox_aid %(AID)s = siox_start_activity( global_unid, %(Description)s );''',
-	'after': '''siox_stop_activity( %(AID)s );
+    'before': '''siox_aid *%(AID)s = (siox_aid*) malloc(sizeof(siox_aid));
+    			 *%(AID)s = siox_start_activity( global_unid, %(TimeStart)s, %(Description)s );''',
+	'after': '''siox_stop_activity( *%(AID)s, %(TimeStop)s );
 			  ''',
-	'cleanup': 'siox_end_activity( %(AID)s );',
+	'cleanup': 'siox_end_activity( *%(AID)s, %(TimeEnd)s );',
 	'final': ''
 },
-# acivity_report
+# activity_link
+# 
+# Links the current activity (started with activity) to another one.
+#
+# AID: AID of the activity to be linked
+# Key: Short description of the activity
+'activity_link': {
+	'variables': 'AID Key',
+	'global': '''''',
+	'init': '''''',
+    'before': '''''',
+	'after': '''g_hash_table_insert(aidHashTable, g_strdup_printf("%i", %(Key)s), (gpointer) %(AID)s );
+    			soix_aid Parent = *(siox_aid*) g_hash_table_lookup(aidHashTable, %(Key)s );
+    			siox_link_activity( *%(AID)s, Parent );
+			  ''',
+	'cleanup': '',
+	'final': ''
+},
+# datatype_register_as_descriptor
+# 
+# Starts (at the beginning) and stops (at the end) a new activity in the current function.
+# Still needs to be reported!
+#
+# AID: Name of the varibale to store the aid
+# Description: Short description of the activity
+'datatype_register_as_descriptor': {
+	'variables': 'DTID',
+	'global': '''''',
+	'init': '''''',
+    'before': '''siox_register_datatype_as_descriptor( %(DTID)s );''',
+	'after': '''''',
+	'cleanup': '',
+	'final': ''
+},
+# report
 #
 # Reports an activity
 #
 # AID: The aid of the activity to be reported
-# Type: Type of the descriptor the activity is dedicated to
-# Descriptor: Pointer to the descriptor
 # MID: Metric-ID of the reported value
 # Value: Pointer to the value to be reported
-# Details: Any explanatory notes
-'activity_report': {
-	'variables': 'AID DescriptorType Descriptor MID value details',
+'report': {
+	'variables': 'AID MID Value',
 	'global': '''''',
 	'init': '''''',
     'before': '''''',
-	'after': 'siox_report_activity(%(AID)s, %(DescriptorType)s, &%(Descriptor)s, %(MID)s, &%(value)s, %(details)s);',
+	'after': 'siox_report( %(AID)s, %(MID)s, (void *) %(Value)s );',
 	'cleanup': '',
 	'final': ''
 },
-# descriptor_map_register
+# report
 #
-# Tells SIOX what types of descriptors can be mapped by this node.
+# Reports an error
 #
-# DmidName: Name of the variable which is used to store the pointer to the dmid
-# Typ1: The type of the origin
-# Typ2: The type of the target
-'descriptor_map_register': {
-	'variables': 'DmidName Typ1 Typ2',
-	'global': '''siox_dmid %(DmidName)s;''',
-	'init': '''%(DmidName)s = siox_register_descriptor_map( global_unid, %(Typ1)s, %(Typ2)s );''',
+# AID: The aid of the activity to be reported
+# Error: The error to be reported
+'report_error': {
+	'variables': 'AID ERROR',
+	'global': '''''',
+	'init': '''''',
     'before': '''''',
+	'after': 'siox_report_error( %(AID)s, %(ERROR)s );',
+	'cleanup': '',
+	'final': ''
+},
+# remote_call_start
+#
+# Initiates a new remote call
+#
+# RCID: The name of the varibale to store the RCID
+# AID: The corresponding AID
+# HWID: The hardware id of the addressed machine
+# SWID: The software id of the addressed machine
+# IID: The instance id of the addressed machine
+'remote_call_start': {
+	'variables': 'RCID AID HWID SWID IID',
+	'global': '''siox_rcid %(RCID)s;\n''',
+	'init': '''''',
+    'before': '''%(RCID)s = siox_describe_remote_call_start( %(AID)s, %(HWID)s, %(SWID)s, %(IID)s );''',
 	'after': '',
 	'cleanup': '',
 	'final': ''
 },
-# descriptor_create
+# remote_call_attribute
 #
-# Creates a new descriptor
+# Send an attribute over a remote call
 #
-# AID: The activity-ID associated
-# Type: The dtid of the descriptor
-# Descriptor: The descriptor
-'descriptor_create': {
-	'variables': 'AID Type Descriptor',
+# RCID: The id of the corresponding remote call
+# DTID: The datatype id of the value
+# Value: Value to be send
+'remote_call_attribute': {
+	'variables': 'RCID DTID Value',
 	'global': '''''',
 	'init': '''''',
-    'before': '''''',
-	'after': 'siox_create_descriptor( %(AID)s, %(Type)s, &%(Descriptor)s);',
-	'cleanup': '',
-	'final': 'siox_release_descriptor( global_unid, %(Type)s, &%(DescriptorName)s);'
-},
-# descriptor_send
-#
-# Sends a descriptor to another node
-#
-# AID: The acitivity associated
-# TargetSWID: The receiving node
-# Type: The type (dtid) of the descriptor associated
-# Descriptor: The descriptor associated
-'descriptor_send': {
-	'variables': 'AID TargetSWID Type Descriptor',
-	'global': '''''',
-	'init': '''''',
-    'before': '''siox_send_descriptor( %(AID)s, %(TargetSWID)s, %(Type)s, &%(Descriptor)s );''',
+    'before': '''siox_remote_call_attribute( %(RCID)s, %(DTID)s, (void *) %(Value)s );''',
 	'after': '',
 	'cleanup': '',
 	'final': ''
 },
-# descriptor_map
+# remote_call_receive
 #
-# Maps a descriptor
+# Receives a remote call
 #
-# AID: The activity-id associated
-# Type: Type of the descriptor
-# OldDescriptor: The old descriptor (to be mapped)
-# NewDescriptor: The new descriptor (emerged from the old descriptor)
-'descriptor_map': {
-	'variables': 'AID Type OldDescriptor NewDescriptor',
-	'global': '''''',
+# AID: The corresponding AID
+# DTID: The datatype id of the value
+# Value: The received value
+'remote_call_receive': {
+	'variables': 'AID DTID Value',
+	'global': '''siox_rcid %(RCID)s;\n''',
 	'init': '''''',
-    'before': '''''',
-	'after': 'siox_map_descriptor( %(AID)s, %(Type)s, &%(OldDescriptor)s, &%(NewDescriptor)s);',
+    'before': '''siox_remote_call_receive( %(RCID)s, %(DTID)s, (void *) %(Value)s );''',
+	'after': '',
 	'cleanup': '',
 	'final': ''
 },
-# descriptor_receive
+# remote_call_end
 #
-# Receives a descriptor
+# Closes a remote call
 #
-# AID: The id of the activity associated
-# Type: The dtid of the descriptor
-# Descriptor: The descriptor
-'descriptor_receive': {
-	'variables': 'AID Type Descriptor',
+# RCID: The RCID of the remote call to be closed
+'remote_call_end': {
+	'variables': 'RCID',
 	'global': '''''',
 	'init': '''''',
-    'before': '''siox_receive_descriptor( %(AID)s, %(Type)s, &%(Descriptor)s );''',
-	'after': '',
+    'before': '''''',
+	'after': 'siox_describe_remote_call_end( %(RCID)s );',
 	'cleanup': '',
 	'final': ''
 },
