652b7ebef67b8abf02a45d9fe8dad8d9e3d7b6eb
Michaela Zimmer <Michaela.Zimmer@Informatik.Uni-Hamburg.de>
2013-03-26 17:29:23 +0100

    First changes on wrapper template.

--- siox-wrapper/template.py
+++ siox-wrapper/template.py
@@ -1,52 +1,72 @@
 template = {
-# register_node
+# component_register
 #
-# Registers (and ungeristers) a new node with SIOX and sets the ontology.
+# Registers (and unregisters) a new component with SIOX and
+# connects to the ontology.
 #
-# SWID: The name (software id) for this node
-'register_node': {
+# SWID: The name (software id) for this component
+'component_register': {
 	'variables': 'SWID',
-	'global': '''siox_unid global_unid;
+	'global': '''siox_component global_component;
+				 siox_ontology global_ontology;
                  char global_pid;
                  char hostname[1024];
 ''',
     'init': '''hostname[1023] = '\\0';
 			   gethostname(hostname, 1023);
                sprintf( &global_pid, "%%d", getpid() );
-               global_unid = siox_register_node(hostname, %(SWID)s, &global_pid);
-               siox_set_ontology("OntologyName");
-               GHashTable *aidHashTable = g_hash_table_new(g_str_hash, g_str_equal);''',
+               global_component = siox_component_register(hostname, %(SWID)s, &global_pid);
+               global_ontology = siox_ontology_connect();
+               GHashTable *activityHashTable = g_hash_table_new(g_str_hash, g_str_equal);''',
 	'before': '',
 	'after': '',
 	'cleanup': '',
-	'final': 'siox_unregister_node(global_unid);'
+	'final': 'siox_component_unregister(global_component);'
 },
-# datatype_register
+# attribute_register
 #
-# Registers a new datatype with siox
+# Registers a new attribute type with siox, so that later calls
+# can set values for this type of attribute on certain entities.
 #
-# RetName: Name of the variable which is used to store the pointer to the dtid
-# Name: Name of the datatype
-# MinStorage: The minimum storage type required to store this datatype
-'datatype_register': {
+# RetName: Name of the variable which is used to store the
+#		   pointer to the attribute type
+# Name: Name of the attribute
+# MinStorage: The minimum storage type required to store this
+#			  attribute
+'attribute_register': {
 	'variables': 'RetName Name MinStorage',
-	'global': '''siox_dtid %(RetName)s;''',
-	'init': '''%(RetName)s = siox_register_datatype( %(Name)s, %(MinStorage)s );''',
+	'global': '''siox_attribute %(RetName)s;''',
+	'init': '''%(RetName)s = siox_attribute_register( %(Name)s, %(MinStorage)s );''',
     'before': '''''',
 	'after': '',
 	'cleanup': '',
 	'final': ''
 },
-# node_attribute
+# component_register_descriptor
 #
-# Reports other important attributes to SIOX
+# Mark an attribute type to serve as a descriptor for this component.
 #
-# DTID: Datatype-ID of the attribute
+# Attribute: Name of the attribute type to mark as a descriptor
+'component_register_descriptor': {
+	'variables': 'Attribute',
+	'global': '''siox_component_register_descriptor( global_component, %(Attribute)s );''',
+	'init': '''''',
+    'before': '''''',
+	'after': '''''',
+	'cleanup': '',
+	'final': ''
+},
+# component_set_attribute
+#
+# Reports the value of one of the component's attributes to SIOX
+#
+# Attribute: Attribute type to set; must have been registered via
+#			 siox_attribute_register().
 # Value: Pointer to the real value of the attribute
-'node_attribute': {
-	'variables': 'DTID Value',
+'component_set_attribute': {
+	'variables': 'Attribute Value',
 	'global': '''''',
-	'init': '''siox_node_attribute( global_unid,  %(DTID)s, %(Value)s);''',
+	'init': '''siox_component_set_attribute( global_component,  %(Attribute)s, %(Value)s);''',
     'before': '''''',
 	'after': '',
 	'cleanup': '',
@@ -54,17 +74,18 @@ template = {
 },
 # metric_register
 #
-# Registers a new metric with SIOX
+# Registers a new metric with SIOX.
+# Metrics are held in an ontology, hence the name.
 #
-# RetName: Name of the variable to store the mid
+# RetName: Name of the variable to store the metric type in
 # Name: Name of the metric
 # UnitType: SIOX unit-type for the metric
 # StorageType: SIOX storage-type for the metric
 # ScopeType: SIOX scope-type for the metric
 'metric_register': {
 	'variables': 'RetName Name UnitType StorageType ScopeType',
-	'global': '''siox_mid %(RetName)s;''',
-	'init': '''%(RetName)s = siox_register_metric( %(Name)s, %(UnitType)s, %(StorageType)s, %(ScopeType)s );''',
+	'global': '''siox_metric %(RetName)s;''',
+	'init': '''%(RetName)s = siox_ontology_register_metric( global_ontology, %(Name)s, %(UnitType)s, %(StorageType)s, %(ScopeType)s );''',
     'before': '''''',
 	'after': '',
 	'cleanup': '',
@@ -72,53 +93,41 @@ template = {
 },
 # activity
 #
-# Starts (at the beginning) and stops (at the end) a new activity in the current function.
+# Starts (at the beginning) and stops (at the end) a new
+# activity in the current function.
 # Still needs to be reported!
 #
-# AID: Name of the varibale to store the aid
+# Activity: Name of the variable to store the activity in
 # TimeStart: Start time to be reported
 # TimeStop: Stop time to be reported
 # TimeEnd: End time to be reported
 # Description: Short description of the activity
 'activity': {
-	'variables': 'AID TimeStart TimeStop TimeEnd Description',
+	'variables': 'Activity TimeStart TimeStop Description',
 	'global': '''''',
 	'init': '''''',
-    'before': '''siox_aid *%(AID)s = (siox_aid*) malloc(sizeof(siox_aid));
-    			 *%(AID)s = siox_start_activity( global_unid, %(TimeStart)s, %(Description)s );''',
-	'after': '''siox_stop_activity( *%(AID)s, %(TimeStop)s );
+    'before': '''siox_activity *%(Activity)s = (siox_activity*) malloc(sizeof(siox_activity));
+    			 *%(Activity)s = siox_activity_start( global_component, %(TimeStart)s, %(Description)s );''',
+	'after': '''siox_stop_activity( *%(Activity)s, %(TimeStop)s );
 			  ''',
-	'cleanup': 'siox_end_activity( *%(AID)s, %(TimeEnd)s );',
+	'cleanup': 'siox_end_activity( *%(Activity)s;',
 	'final': ''
 },
 # activity_link
 #
 # Links the current activity (started with activity) to another one.
 #
-# AID: AID of the activity to be linked
+# Activity: Activity to be linked
 # Key: Short description of the activity
 'activity_link': {
-	'variables': 'AID Key',
+	'variables': 'Activity Key',
 	'global': '''''',
 	'init': '''''',
     'before': '''''',
-	'after': '''g_hash_table_insert(aidHashTable, g_strdup_printf("%i", %(Key)s), (gpointer) %(AID)s );
-    			soix_aid Parent = *(siox_aid*) g_hash_table_lookup(aidHashTable, %(Key)s );
-    			siox_link_activity( *%(AID)s, Parent );
+	'after': '''g_hash_table_insert(activityHashTable, g_strdup_printf("%i", %(Key)s), (gpointer) %(Activity)s );
+    			soix_Activity Parent = *(siox_Activity*) g_hash_table_lookup(activityHashTable, %(Key)s );
+    			siox_link_activity( *%(Activity)s, Parent );
 			  ''',
-	'cleanup': '',
-	'final': ''
-},
-# datatype_register_as_descriptor
-#
-# AID: Name of the varibale to store the aid
-# Description: Short description of the activity
-'datatype_register_as_descriptor': {
-	'variables': 'DTID',
-	'global': '''''',
-	'init': '''''',
-    'before': '''siox_register_datatype_as_descriptor( %(DTID)s );''',
-	'after': '''''',
 	'cleanup': '',
 	'final': ''
 },
@@ -126,30 +135,30 @@ template = {
 #
 # Reports an activity
 #
-# UNID: The unid
-# MID: Metric-ID of the reported value
+# Component: The Component
+# Metric: Metric of the reported value
 # Value: Pointer to the value to be reported
 'report': {
-	'variables': 'UNID MID Value',
+	'variables': 'Component Metric Value',
 	'global': '''''',
 	'init': '''''',
     'before': '''''',
-	'after': 'siox_report( %(UNID)s, %(MID)s, (void *) &%(Value)s );',
+	'after': 'siox_report( %(Component)s, %(Metric)s, (void *) &%(Value)s );',
 	'cleanup': '',
 	'final': ''
 },
-# report
+# report_error
 #
 # Reports an error
 #
-# AID: The aid of the activity to be reported
+# Activity: The Activity to be reported
 # Error: The error to be reported
 'report_error': {
-	'variables': 'AID ERROR',
+	'variables': 'Activity ERROR',
 	'global': '''''',
 	'init': '''''',
     'before': '''''',
-	'after': 'siox_report_error( %(AID)s, %(ERROR)s );',
+	'after': 'siox_report_error( %(Activity)s, %(ERROR)s );',
 	'cleanup': '',
 	'final': ''
 },
@@ -158,15 +167,15 @@ template = {
 # Initiates a new remote call
 #
 # RCID: The name of the varibale to store the RCID
-# AID: The corresponding AID
+# Activity: The corresponding Activity
 # HWID: The hardware id of the addressed machine
 # SWID: The software id of the addressed machine
 # IID: The instance id of the addressed machine
 'remote_call_start': {
-	'variables': 'RCID AID HWID SWID IID',
+	'variables': 'RCID Activity HWID SWID IID',
 	'global': '''siox_rcid %(RCID)s;\n''',
 	'init': '''''',
-    'before': '''%(RCID)s = siox_describe_remote_call_start( %(AID)s, %(HWID)s, %(SWID)s, %(IID)s );''',
+    'before': '''%(RCID)s = siox_describe_remote_call_start( %(Activity)s, %(HWID)s, %(SWID)s, %(IID)s );''',
 	'after': '',
 	'cleanup': '',
 	'final': ''
@@ -191,11 +200,11 @@ template = {
 #
 # Receives a remote call
 #
-# AID: The corresponding AID
+# Activity: The corresponding Activity
 # DTID: The datatype id of the value
 # Value: The received value
 'remote_call_receive': {
-	'variables': 'AID DTID Value',
+	'variables': 'Activity DTID Value',
 	'global': '''siox_rcid %(RCID)s;\n''',
 	'init': '''''',
     'before': '''siox_remote_call_receive( %(RCID)s, %(DTID)s, (void *) &%(Value)s );''',
