9376e541558f453ae4f1a8b58fe859c0ecf799e6
Xuan Wang <xuan.wang@hlrs.de>
2013-04-24 17:24:55 +0200

    Funktioniert version von MPI Wrapper Beispiel

--- siox-wrapper/testing/simple-mpi/compile-wrap.sh
+++ siox-wrapper/testing/simple-mpi/compile-wrap.sh
@@ -8,13 +8,13 @@ python ../../siox-wrapper.py -s wrap -t ./template.py -o $OUTPUT_FILE mpi-wrappe
 
 echo 'Compiling mpi-wrapper.c as mpi-wrapper'
 gcc mpi-wrapper.c -c
-gcc test.c -c
+mpicc test.c -c
 # Appending the output of the wrapper to compile the program
-gcc test.o mpi-wrapper.o -o test_wrap -Wl,--wrap="MPI_Init",--wrap="MPI_File_open",--wrap="MPI_File_set_view",--wrap="MPI_File_read",--wrap="MPI_File_write",--wrap="MPI_File_close",--wrap="MPI_Finalize",--wrap="MPI_Comm_size",--wrap="MPI_Comm_rank"
+mpicc test.o mpi-wrapper.o -o test_wrap -Wl,--wrap="MPI_Init",--wrap="MPI_File_open",--wrap="MPI_File_set_view",--wrap="MPI_File_read",--wrap="MPI_File_write",--wrap="MPI_File_close",--wrap="MPI_Finalize",--wrap="MPI_Comm_size",--wrap="MPI_Comm_rank"
 
 printf '\n---\n\n'
 echo 'Executing test_wrap'
-./mpirun -np 4 test_wrap
+mpirun -np 4 test_wrap
 
 #printf '\n---\n\n'
 #echo -n 'Cat tmp_posix_test: '
