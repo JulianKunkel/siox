359466eb5ed111f8502696e843ffd486e48c8848
Jakob Luettgau <jakob@whatz.eu>
2013-06-24 01:24:35 +0200

    multiplexer sync path

--- monitoring/activity_multiplexer/ActivityMultiplexer_Impl1.cpp
+++ monitoring/activity_multiplexer/ActivityMultiplexer_Impl1.cpp
@@ -2,13 +2,26 @@
 
 #include "ActivityMultiplexer_Impl1.hpp"
 
+using namespace std;
+
 namespace monitoring{
 
+
 void ActivityMultiplexer_Impl1::Log(Activity * element) {
-	std::cout << "Log()\n";
+
+	list<MultiplexerListener<Activity> *>::const_iterator listener;	
+	for ( listener=listeners.begin(); listener!=listeners.end(); ++listener) {
+		(*listener)->Notify(element);
+	}
 }
 
+void ActivityMultiplexer_Impl1::registerListener(MultiplexerListener<Activity> * listener){
+	listeners.push_back(listener);		
+}
 
+void ActivityMultiplexer_Impl1::unregisterListener(MultiplexerListener<Activity> * listener){
+	listeners.remove(listener);
+}
 
 }
 
--- monitoring/activity_multiplexer/ActivityMultiplexer_Impl1.hpp
+++ monitoring/activity_multiplexer/ActivityMultiplexer_Impl1.hpp
@@ -12,17 +12,32 @@
 /*!
  Software Structure
  1 Requirements
+
+
+
  2 UseCases
  3 Design und Text
  4 Testimplementation
+ 
+ 5 Considerations
+	Removing a listener might invalidate iterator
+	Inserting is usually fine (for lists uneffected, vectors until resize)
+
+	Solutions:
+	1.	snapshot list	-> memory + time panelty
+
+	2.	change remove to null, check when iterating, 
+		clean up in additional loop, might skip if time is precious
+
  */
 
 /*! The detached threads for notify are running until the SIOX system finishes.
  When the instrumented client program finishes, a SIOX_finalize() call will be issued that sets a flag called "thrd_fin".
  */
-// that flag is global?
 #ifndef ACTIVITYMULTIPLEXER_IMPL1_H
 #define ACTIVITYMULTIPLEXER_IMPL1_H 
+
+#include <list>
 
 #include <monitoring/activity_multiplexer/ActivityMultiplexer.hpp>
 #include <monitoring/activity_multiplexer/ActivityMultiplexerListener.hpp>
@@ -32,7 +47,12 @@ namespace monitoring{
 class ActivityMultiplexer_Impl1 : public ActivityMultiplexer
 {
 public:
-	void Log(Activity * element);
+	virtual void Log(Activity * element);
+	virtual void registerListener(MultiplexerListener<Activity> * listener);	
+	virtual void unregisterListener(MultiplexerListener<Activity> * listener);
+
+private:
+	list<MultiplexerListener<Activity> *> listeners;
 };
 
 }
