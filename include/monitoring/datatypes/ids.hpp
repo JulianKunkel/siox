dee0ab6d7e54728c35e0fd122cc1931357285048
Holger Mickler <holger.mickler@tu-dresden.de>
2013-07-24 13:38:35 +0200

    Moved RemoteCallID below ActivityID and removed forward declaration which is now obsolete.

--- include/monitoring/datatypes/ids.hpp
+++ include/monitoring/datatypes/ids.hpp
@@ -49,9 +49,6 @@ typedef struct {
 	AttributeValue value;
 } Attribute;
 
-/* Forward declaration */
-struct ActivityID;
-
 // The daemon fetches the NodeID from the knowledge base (or initiates creation if necessary)
 // NodeID lookup_node_id(const char * hostname);
 // See @TODO 
@@ -107,15 +104,6 @@ typedef struct{
 // TODO:
 // ComponentID(siox_component){}
 // ComponentID create_component_id(ProcessID 3*32 B, UIID + 16 bit);
-// The instance identifier such as "Port 4711" is relevant for matching of remote calls
-// See @TODO
-
-typedef struct {
-	ActivityID *caller_aid;
-	NodeID *target_node_id;
-	UniqueInterfaceID *target_unique_interface_id;
-	AssociateID *target_associate_id;
-} RemoteCallID;
 
 typedef struct {
 	// Several parameters assist matching of remote calls
@@ -137,6 +125,15 @@ typedef struct ActivityID{
 
 // ActivityID create_activity_id(ComponentID 4*32 B, <Incrementing Counter>);
 // See @TODO
+
+// The instance identifier such as "Port 4711" is relevant for matching of remote calls
+// See @TODO
+typedef struct {
+	ActivityID *caller_aid;
+	NodeID *target_node_id;
+	UniqueInterfaceID *target_unique_interface_id;
+	AssociateID *target_associate_id;
+} RemoteCallID;
 
 // The identifier is fetched from the knowledge base
 // OntologyAttributeID lookup_ontology_attribute(string uniqueOntologyIdentifier);
