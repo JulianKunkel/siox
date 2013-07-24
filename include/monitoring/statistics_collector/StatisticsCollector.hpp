916c6d113ac341985f7e2b07eca29533c89a13b4
Marc Wiedemann <marc.wiedemann@informatik.uni-hamburg.de>
2013-07-24 11:33:36 +0200

    continued StatisticsCollector

--- include/monitoring/statistics_collector/StatisticsCollector.hpp
+++ include/monitoring/statistics_collector/StatisticsCollector.hpp
@@ -8,14 +8,15 @@ namespace monitoring{
 class StatisticsCollector {
 public:
 
-	virtual void register_metrics(StatisticsValue * statvalue);
+	virtual void register_metrics(String metricname, string type, string domain, string hostname, string si_unit, string description, uint32_t min_poll_interval_ms);
+// Hier kann auch der predefined variant-Datatype MetricAttributes genutzt werden.
 
 // Description  means local or remote metric
 // Type = gauge if interval - We are converting incremental to gauge
 
-	virtual void get_metrics(String metricname, string type, string domain, string hostname, string si_unit, string description, uint32_t min_poll_interval_ms);
+	virtual void get_metrics(StatisticsValue * statvalue);
 
-// Hier Datatype MetricAttributes
+
 
 };
 
