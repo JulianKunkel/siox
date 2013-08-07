acc0b742181dda1c01a40886e128520cb9e2f264
Julian Kunkel <juliankunkel@googlemail.com>
2013-08-07 16:40:22 +0200

    Allow sourcing of the shell script multiple times without blowing the number of paths.

--- set_ld_path.sh
+++ set_ld_path.sh
@@ -4,7 +4,10 @@ echo "# This script sets the LD_LIBRARY_PATH to include all shared libraries."
 echo "# This is very useful for debugging of executables in build/"
 echo "# Just source $0"
 
-D="$LD_LIBRARY_PATH"
+if [[ "$OLD_LD_LIBRARY_PATH" == "" ]] ; then
+	export OLD_LD_LIBRARY_PATH=":$LD_LIBRARY_PATH"
+fi
+D="$OLD_LD_LIBRARY_PATH"
 for X in  `(for I in $(find build -name "*.so") ; do dirname $I ; done)|uniq` ;  do
 D="$D:$PWD/$X"
 done
