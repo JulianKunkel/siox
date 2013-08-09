8268af25f1a434ebb39062352d95549fa71301da
Marc Wiedemann <marc.wiedemann@informatik.uni-hamburg.de>
2013-08-09 11:16:13 +0200

    test instance

--- monitoring/statistics_collector/test/ThreadedStatisticsCollectorTest.cpp
+++ monitoring/statistics_collector/test/ThreadedStatisticsCollectorTest.cpp
@@ -7,9 +7,20 @@
 
 
 
+void init(){
+
+};
+
+void destroy(){
+
+};
+
 
 int main(int argc, char const *argv[]){
-		
+	StatisticsCollector * tsc = core::module_create_instance<StatisticsCollector>("", "ThreadedStatisticsCollector", THREADED_STATISTICS_COLLECTOR_INTERFACE);
+	assert(o != nullptr);
+	ThreadedStatisticsCollectorOptions & op = tsc->getOptions<ThreadedStatisticsCollectorOptions>();
+
 	}
 
 	cout << "OK" << endl;	
