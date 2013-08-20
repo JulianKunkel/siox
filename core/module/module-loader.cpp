3ef17241aae5dc13a2764dee5861565e056c9843
Nathanael Hübbe <nathanael.huebbe@informatik.uni-hamburg.de>
2013-08-20 17:59:09 +0200

    style

--- core/module/module-loader.cpp
+++ core/module/module-loader.cpp
@@ -8,12 +8,11 @@
 
 namespace core {
 
-ModuleError::ModuleError(string module_path, string module_name, 
-			 string interface, string msg) 
+ModuleError::ModuleError(string module_path, string module_name, string interface, string msg)
 {
 	this->interface = interface;
 	this->msg = msg;
-	
+
 	if (module_path != "")
 		this->module = module + "/" + module_name + ".so";
 	else
@@ -23,40 +22,36 @@ ModuleError::ModuleError(string module_path, string module_name,
 }
 
 
-const char *ModuleError::what() const throw() 
+const char *ModuleError::what() const throw()
 {
 	return this->msg.c_str();
 }
 
 namespace module_internal {
 
-void *module_create_instance(string module_path, string module_name, 
-			     string interface_name) throw (ModuleError) 
+void *module_create_instance(string module_path, string module_name, string interface_name) throw (ModuleError)
 {
 	// check basic consistency of the arguments
 	assert(module_name != "");
 	assert(interface_name != "");
 
-	string fullQualifiedFileName = (module_path != "") ? module_path + 
-		"/lib" + module_name + ".so" : "lib" + module_name + ".so";
-		
+	string fullQualifiedFileName = (module_path != "") ? module_path + "/lib" + module_name + ".so" : "lib" + module_name + ".so";
+
 	GModule *module = g_module_open(fullQualifiedFileName.c_str(), G_MODULE_BIND_LAZY);
 
 	if (module == nullptr) {
 		const char * errmsg = g_module_error();
 		throw ModuleError(module_path, module_name, interface_name, errmsg);
-	} 
+	}
 
 	// lookup symbol which will create the instance
 
 	void *(*module_instance_function)();
 	string instance_fkt_name = INSTANCE_FKT_PREFIX + interface_name;
-	  
-	if (!g_module_symbol(module, instance_fkt_name.c_str(), 
-	    (gpointer*) &module_instance_function )) {
-		throw ModuleError(module_path, module_name, interface_name, 
-				  "Instance function " + instance_fkt_name  + 
-				  "() not implemented. Are you sure this module offers the interface you are looking for?");	 
+
+	if (!g_module_symbol(module, instance_fkt_name.c_str(), (gpointer*) &module_instance_function )) {
+		throw ModuleError(module_path, module_name, interface_name,
+		                  "Instance function " + instance_fkt_name + "() not implemented. Are you sure this module offers the interface you are looking for?");
 	}
 
 	// create the instance:
@@ -65,9 +60,8 @@ void *module_create_instance(string module_path, string module_name,
 	if (instance != nullptr) {
 		return instance;
 	}
-	
-	throw ModuleError(module_path, module_name, interface_name, 
-			  "Instance function returned a null pointer.");
+
+	throw ModuleError(module_path, module_name, interface_name, "Instance function returned a null pointer.");
 }
 
 }
