1ee8a2a417fd1e40e6a5119505a6a0902f67b6d6
Alvaro Aguilera <alvaro.aguilera@tu-dresden.de>
2013-09-03 13:01:07 +0200

    CTest file + some refractoring.

---
+++ CTestConfig.cmake
@@ -0,0 +1,8 @@
+set(CTEST_PROJECT_NAME "SIOX")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=SIOX")
+set(CTEST_DROP_SITE_CDASH TRUE)
+set(CTEST_MEMORYCHECK_COMMAND valgrind)
+set(CTEST_MEMORYCHECK_COMMAND_OPTIONS "-v --tool=memcheck --leak-check=full --track-fds=yes --num-callers=50 --show-reachable=yes --track-origins=yes --malloc-fill=0xff --free-fill=0xfe")
