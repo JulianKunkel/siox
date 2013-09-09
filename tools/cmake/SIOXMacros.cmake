faa2538dda14fb4b4bc92f5026cec7f4bf6bf47d
Alvaro Aguilera <alvaro.aguilera@tu-dresden.de>
2013-09-09 16:52:36 +0200

    Some refactored code.

--- tools/cmake/SIOXMacros.cmake
+++ tools/cmake/SIOXMacros.cmake
@@ -1,5 +1,6 @@
 macro(SIOX_GENERATE_BUFFERS)
 	PROTOBUF_GENERATE_CPP(PROTO_SRCS PROTO_HDRS ${SIOX_INCLUDE}/core/comm/messages/siox.proto)
-	add_library(core-commm-buffers ${PROTO_SRCS} ${PROTO_HDRS})
+	add_library(core-comm-buffers ${PROTO_SRCS} ${PROTO_HDRS})
+	target_link_libraries(core-comm-buffers ${PROTOBUF_LIBRARIES})
 endmacro(SIOX_GENERATE_BUFFERS)
 
