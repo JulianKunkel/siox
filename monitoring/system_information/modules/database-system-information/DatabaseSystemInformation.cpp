61a1364ff9e5ac822bb43afa1d7e48a4445bdf6c
Roman Michel <rmi6chr@gmail.com>
2013-09-12 16:38:34 +0200

    Further work on DatabaseSystemInformation.cpp

--- monitoring/system_information/modules/database-system-information/DatabaseSystemInformation.cpp
+++ monitoring/system_information/modules/database-system-information/DatabaseSystemInformation.cpp
@@ -23,8 +23,6 @@ namespace monitoring {
 	auto res = map.find(srch); \
 	if (res != map.end()) { \
 		return res->second;\
-	} else {\
-		throw NotFoundError();\
 	}
 
 	class DatabaseSystemInformation: public SystemInformationGlobalIDManager {
@@ -56,7 +54,33 @@ namespace monitoring {
 				return id;
 			}
 
-			// TODO: id aus der Datenbank holen und füllen
+	        // Create a new transaction. It gets automatically destroyed at the end of this funtion.
+	        work insertAction(*conn, "Insert Transaction");
+
+	        // Perform the insert
+	        insertAction.exec("INSERT INTO NodeMapping (Hostname) VALUES ('"+insertAction.esc(hostname)+"')");
+	        insertAction.commit();
+
+	        work selectAction(*conn, "Select Transaction");
+
+		    // Perform a select
+		    result resultSelect = selectAction.exec(("SELECT NodeID FROM NodeMapping WHERE Hostname='" + selectAction.esc(hostname) + "'"));
+		    selectAction.commit();
+
+		    // Check if there is only one result
+		    if (resultSelect.size() == 1) {
+		        // Check if results are sane (and convert them)
+		        // TODO: Conversion okay?
+		        if (!resultSelect[0]["NodeID"].to(id)) {
+		            throw integretyError;
+		            return NULL;
+		        }
+		    }
+		    else {
+		        // TODO: ERROR
+		        return NULL;
+		    }
+
 			
 			m.lock();
 			valueStringMap[id] = hostname;
@@ -69,13 +93,55 @@ namespace monitoring {
 		NodeID lookup_nodeID( const string & hostname ) const throw( NotFoundError ) {
 			CHECK( nodeMap, hostname )
 
-			// TODO: Falls CHECK fehlschlägt: Datenbank abfragen, dann erst exception
+			NodeID id;
+
+	        work selectAction(*conn, "Select Transaction");
+
+		    // Perform a select
+		    result resultSelect = selectAction.exec(("SELECT NodeID FROM NodeMapping WHERE Hostname='" + selectAction.esc(hostname) + "'"));
+		    selectAction.commit();
+
+		    // Check if there is only one result
+		    if (resultSelect.size() == 1) {
+		        // Check if results are sane (and convert them)
+		        // TODO: Conversion okay?
+		        if (!resultSelect[0]["NodeID"].to(id)) {
+		            throw integretyError;
+		            return NULL;
+		        }
+		    }
+		    else {
+				throw NotFoundError();
+				return NULL;
+		    }
 		}
 
 		const string & lookup_node_hostname( NodeID id ) const throw( NotFoundError ) {
 			CHECK( valueStringMap, id )
 
-			// TODO: Falls CHECK fehlschlägt: Datenbank abfragen, dann erst exception
+			NodeID id;
+
+	        work selectAction(*conn, "Select Transaction");
+	        string sqlstring;
+
+		    // Perform a select
+		    sprintf(sqlstring, "SELECT Hostname FROM NodeMapping WHERE NodeID='%d'", id)
+		    result resultSelect = selectAction.exec((sqlstring));
+		    selectAction.commit();
+
+		    // Check if there is only one result
+		    if (resultSelect.size() == 1) {
+		        // Check if results are sane (and convert them)
+		        // TODO: Conversion okay?
+		        if (!resultSelect[0]["NodeID"].to(id)) {
+		            throw integretyError;
+		            return NULL;
+		        }
+		    }
+		    else {
+				throw NotFoundError();
+				return NULL;
+		    }			
 		}
 
 		DeviceID register_deviceID( NodeID id, const  string & local_unique_identifier ) {
@@ -86,9 +152,38 @@ namespace monitoring {
 				return device_id;
 			}
 
-			// TODO: Datenbank query'en
+	        // Create a new transaction. It gets automatically destroyed at the end of this funtion.
+	        work insertAction(*conn, "Insert Transaction");
+
+	        string sqlstring;
+	        sprintf(sqlstring, "INSERT INTO DeviceMapping (UniqueIdentifier, NodeID) VALUES ('"+insertAction.esc(local_unique_identifier)+"', '%d')", id);
+
+	        // Perform the insert
+	        insertAction.exec(sqlstring);
+	        insertAction.commit();
+
+	        work selectAction(*conn, "Select Transaction");
+	        sprintf(sqlstring, "SELECT DeviceID FROM DeviceMapping WHERE UniqueIdentifier='" + selectAction.esc(local_unique_identifier) + "' AND NodeID = '%d'", id);
+
+		    // Perform a select
+		    result resultSelect = selectAction.exec(sqlstring);
+		    selectAction.commit();
+
+		    // Check if there is only one result
+		    if (resultSelect.size() == 1) {
+		        // Check if results are sane (and convert them)
+		        // TODO: Conversion okay?
+		        if (!resultSelect[0]["DeviceID"].to(device_id)) {
+		            throw integretyError;
+		            return NULL;
+		        }
+		    }
+		    else {
+		        // TODO: ERROR
+		        return NULL;
+		    }
+			
 			m.lock();
-			device_id = nextID++;
 			deviceMap[pair_str] = device_id;
 			valueStringMap[device_id] = local_unique_identifier;
 			deviceNodeMap[device_id] = id;
@@ -102,19 +197,83 @@ namespace monitoring {
 			auto pair_str = pair<uint32_t, string>( id, local_unique_identifier );
 			CHECK( deviceMap, pair_str )
 
-			// TODO: Falls CHECK fehlschlägt: Datenbank abfragen, dann erst exception
+			work selectAction(*conn, "Select Transaction");
+	        sprintf(sqlstring, "SELECT DeviceID FROM DeviceMapping WHERE UniqueIdentifier='" + selectAction.esc(local_unique_identifier) + "' AND NodeID = '%d'", id);
+
+		    // Perform a select
+		    result resultSelect = selectAction.exec(sqlstring);
+		    selectAction.commit();
+
+		    // Check if there is only one result
+		    if (resultSelect.size() == 1) {
+		        // Check if results are sane (and convert them)
+		        // TODO: Conversion okay?
+		        if (!resultSelect[0]["DeviceID"].to(device_id)) {
+		            throw integretyError;
+		            return NULL;
+		        }
+		        return device_id;
+		    }
+		    else {
+		        throw NotFoundError();
+		        return NULL;
+		    }
 		}		
 
 		NodeID lookup_node_of_device( DeviceID id ) const throw( NotFoundError ) {
 			CHECK( deviceNodeMap, id )
 
-			// TODO: Falls CHECK fehlschlägt: Datenbank abfragen, dann erst exception
+			NodeID node_id;
+
+			work selectAction(*conn, "Select Transaction");
+	        sprintf(sqlstring, "SELECT NodeID FROM DeviceMapping WHERE UniqueIdentifier='" + selectAction.esc(local_unique_identifier) + "' AND DeviceID = '%d'", id);
+
+		    // Perform a select
+		    result resultSelect = selectAction.exec(sqlstring);
+		    selectAction.commit();
+
+		    // Check if there is only one result
+		    if (resultSelect.size() == 1) {
+		        // Check if results are sane (and convert them)
+		        // TODO: Conversion okay?
+		        if (!resultSelect[0]["NodeID"].to(node_id)) {
+		            throw integretyError;
+		            return NULL;
+		        }
+		        return node_id;
+		    }
+		    else {
+		        throw NotFoundError();
+		        return NULL;
+		    }
 		}		
 
 		const string & lookup_device_local_name( DeviceID id ) const throw( NotFoundError ) {
 			CHECK( valueStringMap, id )
 
-			// TODO: Falls CHECK fehlschlägt: Datenbank abfragen, dann erst exception
+			string uid;
+
+			work selectAction(*conn, "Select Transaction");
+	        sprintf(sqlstring, "SELECT UniqueIdentifier FROM DeviceMapping WHERE DeviceID = '%d'", id);
+
+		    // Perform a select
+		    result resultSelect = selectAction.exec(sqlstring);
+		    selectAction.commit();
+
+		    // Check if there is only one result
+		    if (resultSelect.size() == 1) {
+		        // Check if results are sane (and convert them)
+		        // TODO: Conversion okay?
+		        if (!resultSelect[0]["UniqueIdentifier"].to(uid)) {
+		            throw integretyError;
+		            return NULL;
+		        }
+		        return uid;
+		    }
+		    else {
+		        throw NotFoundError();
+		        return NULL;
+		    }
 		}		
 
 		FilesystemID register_filesystemID( const string & global_unique_identifier ) {
@@ -123,9 +282,38 @@ namespace monitoring {
 				return fs_id;
 			}
 
-			// TODO: Datenbank
+	        // Create a new transaction. It gets automatically destroyed at the end of this funtion.
+	        work insertAction(*conn, "Insert Transaction");
+
+	        string sqlstring;
+	        sprintf(sqlstring, "INSERT INTO FileystemMapping (UniqueIdentifier) VALUES ('"+insertAction.esc(global_unique_identifier)+"')");
+
+	        // Perform the insert
+	        insertAction.exec(sqlstring);
+	        insertAction.commit();
+
+	        work selectAction(*conn, "Select Transaction");
+	        sprintf(sqlstring, "SELECT FilesystemID FROM FileystemMapping WHERE UniqueIdentifier='" + selectAction.esc(global_unique_identifier) + "'");
+
+		    // Perform a select
+		    result resultSelect = selectAction.exec(sqlstring);
+		    selectAction.commit();
+
+		    // Check if there is only one result
+		    if (resultSelect.size() == 1) {
+		        // Check if results are sane (and convert them)
+		        // TODO: Conversion okay?
+		        if (!resultSelect[0]["FilesystemID"].to(fs_id)) {
+		            throw integretyError;
+		            return NULL;
+		        }
+		    }
+		    else {
+		        // TODO: ERROR
+		        return NULL;
+		    }
+
 			m.lock();
-			fs_id = nextID++;
 			filesystemMap[global_unique_identifier] = fs_id;
 			valueStringMap[fs_id] = global_unique_identifier;
 			m.unlock();
@@ -136,13 +324,57 @@ namespace monitoring {
 		FilesystemID lookup_filesystemID( const string & global_unique_identifier ) const throw( NotFoundError ) {
 			CHECK( filesystemMap, global_unique_identifier )
 
-			// TODO: Falls CHECK fehlschlägt: Datenbank abfragen, dann erst exception
+			FilesystemID fsid;
+
+			work selectAction(*conn, "Select Transaction");
+	        sprintf(sqlstring, "SELECT FilesystemID FROM FileystemMapping WHERE UniqueIdentifier = '%s'", global_unique_identifier);
+
+		    // Perform a select
+		    result resultSelect = selectAction.exec(sqlstring);
+		    selectAction.commit();
+
+		    // Check if there is only one result
+		    if (resultSelect.size() == 1) {
+		        // Check if results are sane (and convert them)
+		        // TODO: Conversion okay?
+		        if (!resultSelect[0]["FilesystemID"].to(fsid)) {
+		            throw integretyError;
+		            return NULL;
+		        }
+		        return fsid;
+		    }
+		    else {
+		        throw NotFoundError();
+		        return NULL;
+		    }
 		}		
 
 		const string & lookup_filesystem_name( FilesystemID id ) const throw( NotFoundError ) {
 			CHECK( valueStringMap, id )
 
-			// TODO: Falls CHECK fehlschlägt: Datenbank abfragen, dann erst exception
+			string fsname;
+
+			work selectAction(*conn, "Select Transaction");
+	        sprintf(sqlstring, "SELECT UniqueIdentifier FROM FileystemMapping WHERE FilesystemID = '%d'", id);
+
+		    // Perform a select
+		    result resultSelect = selectAction.exec(sqlstring);
+		    selectAction.commit();
+
+		    // Check if there is only one result
+		    if (resultSelect.size() == 1) {
+		        // Check if results are sane (and convert them)
+		        // TODO: Conversion okay?
+		        if (!resultSelect[0]["UniqueIdentifier"].to(fsname)) {
+		            throw integretyError;
+		            return NULL;
+		        }
+		        return fsname;
+		    }
+		    else {
+		        throw NotFoundError();
+		        return NULL;
+		    }
 		}
 
 		UniqueInterfaceID register_interfaceID( const string & interface, const string & implementation ) {
@@ -155,13 +387,38 @@ namespace monitoring {
 				return cur;
 			}
 
-			// check if interface exists
+	        // Create a new transaction. It gets automatically destroyed at the end of this funtion.
+	        work insertAction(*conn, "Insert Transaction");
 
-			// TODO: Datenbank
+	        string sqlstring;
+	        sprintf(sqlstring, "INSERT INTO UniqueInterfaceMapping (Implementation, Interface) VALUES ('"+insertAction.esc(interface)+"', '"+insertAction.esc(implementation)+"')");
+
+	        // Perform the insert
+	        insertAction.exec(sqlstring);
+	        insertAction.commit();
+
+	        work selectAction(*conn, "Select Transaction");
+	        sprintf(sqlstring, "SELECT UniqueInterfaceID FROM UniqueInterfaceMapping WHERE Implementation='" + selectAction.esc(global_unique_identifier) + "' AND Interface = '"+insertAction.esc(implementation)+"'");
+
+		    // Perform a select
+		    result resultSelect = selectAction.exec(sqlstring);
+		    selectAction.commit();
+
+		    // Check if there is only one result
+		    if (resultSelect.size() == 1) {
+		        // Check if results are sane (and convert them)
+		        // TODO: Conversion okay?
+		        if (!resultSelect[0]["UniqueInterfaceID"].to(cur)) {
+		            throw integretyError;
+		            return NULL;
+		        }
+		    }
+		    else {
+		        // TODO: ERROR
+		        return NULL;
+		    }
+
 			m.lock();
-
-			cur = nextID++;
-
 			interfaceImplMap[pair_str] = cur;
 			interfaceImplStrMap[cur] = pair_str;
 
