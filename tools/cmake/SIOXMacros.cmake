b3f3383594d213b7a0698bf4d0e87b505a2b96c5
Julian Kunkel <juliankunkel@googlemail.com>
2013-09-18 18:59:59 +0200

    CMake extended, e.g. to integrate serialization as used in waf. Does combile now but misses certain files.

--- tools/cmake/SIOXMacros.cmake
+++ tools/cmake/SIOXMacros.cmake
@@ -11,3 +11,22 @@ macro(SIOX_RUN_SERIALIZER IN OUT)
 	ARGS -I ${CMAKE_SOURCE_DIR}/include -i ${CMAKE_CURRENT_SOURCE_DIR}/${IN} -o ${OUT} 
     )
 endmacro(SIOX_RUN_SERIALIZER)
+
+
+macro(SIOX_RUN_BINARY_SERIALIZER IN OUT)
+    add_custom_command(
+	OUTPUT ${OUT}
+	COMMAND ${CMAKE_SOURCE_DIR}/tools/serialization-code-generator.py
+	ARGS -s JBinary -I ${CMAKE_SOURCE_DIR}/include -i ${CMAKE_CURRENT_SOURCE_DIR}/${IN} -o ${OUT} 
+    )
+endmacro(SIOX_RUN_BINARY_SERIALIZER)
+
+ macro(SIOX_RUN_TEXT_SERIALIZER IN OUT)
+    add_custom_command(
+	OUTPUT ${OUT}
+	COMMAND ${CMAKE_SOURCE_DIR}/tools/serialization-code-generator.py
+	ARGS -f text -I ${CMAKE_SOURCE_DIR}/include -i ${CMAKE_CURRENT_SOURCE_DIR}/${IN} -o ${OUT} 
+    )
+endmacro(SIOX_RUN_TEXT_SERIALIZER)
+
+ 
