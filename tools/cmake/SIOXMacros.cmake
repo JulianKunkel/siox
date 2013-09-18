9eba1a712a67a60fb48f4694fefb5e909f1bd28a
Alvaro Aguilera <alvaro.aguilera@tu-dresden.de>
2013-09-18 12:52:04 +0200

    Macro for Python-Serializer + example for the Activity Multiplexer.

--- tools/cmake/SIOXMacros.cmake
+++ tools/cmake/SIOXMacros.cmake
@@ -4,3 +4,10 @@ macro(SIOX_GENERATE_BUFFERS)
 	target_link_libraries(core-comm-buffers ${PROTOBUF_LIBRARIES})
 endmacro(SIOX_GENERATE_BUFFERS)
 
+macro(SIOX_RUN_SERIALIZER IN OUT)
+    add_custom_command(
+	OUTPUT ${OUT}
+	COMMAND ${CMAKE_SOURCE_DIR}/tools/serialization-code-generator.py
+	ARGS -I ${CMAKE_SOURCE_DIR}/include -i ${CMAKE_CURRENT_SOURCE_DIR}/${IN} -o ${OUT} 
+    )
+endmacro(SIOX_RUN_SERIALIZER)
