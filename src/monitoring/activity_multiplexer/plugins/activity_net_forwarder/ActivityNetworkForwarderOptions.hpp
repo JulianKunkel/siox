fa78c6221d5ef484a1ee86149fd301abd47571f4
Marc Wiedemann <marc.wiedemann@informatik.uni-hamburg.de>
2013-12-19 16:34:00 +0100

    Documenting

--- src/monitoring/activity_multiplexer/plugins/activity_net_forwarder/ActivityNetworkForwarderOptions.hpp
+++ src/monitoring/activity_multiplexer/plugins/activity_net_forwarder/ActivityNetworkForwarderOptions.hpp
@@ -8,12 +8,15 @@ using namespace monitoring;
 //@serializable
 class ActivityNetworkForwarderClientOptions: public ActivityMultiplexerPluginOptions {
 	public:
+		// Use communication component with specified target address
 		ComponentReference comm;
 		string targetAddress;
 
+		// When the forwardAllActivities is set to false, all the Activities are sent to the reasoner by the AN forwarder client
 		bool forwardAllActivities;
 		ComponentReference reasoner;
 
+		// Ringbuffersize unit is bytes
 		uint ringBufferSize = 0;
 };
 
@@ -22,8 +25,10 @@ class ActivityNetworkForwarderClientOptions: public ActivityMultiplexerPluginOpt
 //@serializable
 class ActivityNetworkForwarderServerOptions: public ComponentOptions {
 	public:
+		// Options for target multiplexer empty at the moment
 		ComponentReference target_multiplexer;
 
+		// Use communication component with specified target address
 		ComponentReference comm;
 		string serviceAddress;		
 };
