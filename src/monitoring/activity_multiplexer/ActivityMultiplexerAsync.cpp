498e3271b83cacb5bb81c150fe4f4342d012ce62
Nathanael Hübbe <nathanael.huebbe@informatik.uni-hamburg.de>
2014-02-05 17:16:22 +0100

    Optimized the ActivityMultiplexerAsync. Also removed  from the CMakeLists.txt as this prooved to be a significant performance burner. Can now process 450,000 activities per second with no compiler optimization, over a million with optimization switched on.

--- src/monitoring/activity_multiplexer/ActivityMultiplexerAsync.cpp
+++ src/monitoring/activity_multiplexer/ActivityMultiplexerAsync.cpp
@@ -18,6 +18,7 @@
 #include <deque>
 #include <mutex>
 #include <condition_variable>
+#include <atomic>
 
 #include <thread>
 
@@ -30,7 +31,6 @@
 
 
 #include "ActivityMultiplexerAsyncOptions.hpp"
-
 
 using namespace core;
 using namespace monitoring;
@@ -50,7 +50,7 @@ namespace monitoring {
 
 
 	/**
-	 * A threadsafe queue implementation for the multiplexer to use
+	 * A threadsafe (many producers, one consumer) queue implementation for the multiplexer to use
 	 * The queue is also responsible for counting discarded activities.
 	 */
 	class ActivityMultiplexerQueue
@@ -60,49 +60,29 @@ namespace monitoring {
 		std::mutex lock;
 		std::condition_variable not_empty;
 
-		std::deque< shared_ptr<Activity> > queue;
-		unsigned int capacity = 10; // TODO specify by options
+		const static int capacityExponent = 5;
+		const static int capacity = 1 << capacityExponent;
+		const static int indexMask = capacity - 1;
+		shared_ptr<Activity> buffer[capacity];	//Writing is protected by `lock`, reading is protected by the values of `writeIndex` and `readIndex`.
+		std::atomic<uint32_t> writeIndex, readIndex;	//Writing of `writeIndex` is protected by `lock`. Bits outside of the indexMask are undefined, and must be ignored when these variables are interpreted.
+//		std::deque< shared_ptr<Activity> > queue;
+//		unsigned int capacity = 10; // TODO specify by options
 
 		bool overloaded = false;
-		int lost = 1;
-
-		// old mutex
-		std::mutex mut;
-
-		bool terminate = false;
+		std::atomic<int> lost;
+		std::atomic<bool> notified;
+		std::atomic<bool> terminate;
+		std::atomic<bool> terminated;
 
 
 	public:
-			ActivityMultiplexerQueue () {};
+			ActivityMultiplexerQueue () : writeIndex(0), readIndex(0), lost(0), notified(false), terminate(false), terminated(false) {};
 			virtual ~ActivityMultiplexerQueue () {};
 
-			/**
-			 * Check whether or not the queue has still capacity
-			 *
-			 * @return  bool	true = queue is full, false = not full
-			 */
-			virtual bool Full() {
-				return ( queue.size() > capacity );
+			virtual bool isEmpty() {
+				return !(indexMask & (writeIndex - readIndex));
 			};
 
-
-
-			/* Check whether of not the queue is empty
-			 *
-			 * @return bool     true = queue is empty, false = not empty
-			 */
-			virtual bool Empty() {
-				return queue.empty();
-			};
-
-
-			/**
-			 * Check if queue is in overload mode
-			 *
-			 */
-			virtual bool Overloaded() {
-				return overloaded;
-			};
 
 			/**
 			 * Add an activity to the queue if there is capacity, set overload flag
@@ -110,62 +90,66 @@ namespace monitoring {
 			 *
 			 * @param   activity     an activity that need to be dispatched in the future
 			 */
-			virtual void Push(shared_ptr<Activity> activity) {
-				//std::lock_guard<std::mutex> lock( mut );
-
-				std::unique_lock<std::mutex> l(lock);
-
-				if ( Overloaded() ) {
+			virtual void Push( shared_ptr<Activity> activity ) {
+				if( overloaded ) {
 					lost++;
 					return;
 				}
-
-				//printf("push %p\n", activity);
-
-				if( ! Full() ) {
-					queue.push_back( activity );
-					if( queue.size() == 1 ){
-						//cout << "NOTIFYING" << endl;
-						not_empty.notify_one();
+				lock.lock();
+					size_t curSize = indexMask & ( writeIndex - readIndex );
+					//We cannot distinguish between the case that the two pointers are equal due to an empty or a full buffer, so we need to keep one slot empty.
+					if( curSize < capacity - 1 ) {
+						buffer[indexMask & writeIndex] = activity;
+						std::atomic_thread_fence(std::memory_order_release);
+						writeIndex++;
+					}else{
+						overloaded = true;
+						lost++;
 					}
-				}else{
-					//cout << "OVERLOADED!" << endl;
-					overloaded = true;
-					lost = 1;
-				}				
+				lock.unlock();
+				if( curSize > capacity/2 && !notified ){
+					//Signaling a condition variable is extremely expensive. Don't do this for every Activity we get.
+					not_empty.notify_one();
+				}
 			};
 
 
 			/**
-			 * Get an activity from queue, returned element is popped!
+			 * Get an activity from queue, returned element is popped.
+			 * If this returns a null pointer, the caller must immediately forget that this queue ever existed - it might already be destructed.
 			 *
-			 * @return	Activity	an activity that needs to be dispatched to async listeners
+			 * @return	Activity	an activity that needs to be dispatched to async listeners, or NULL if the queue has been finalized.
 			 */
 			virtual shared_ptr<Activity> Pop() {
-				std::unique_lock<std::mutex> l(lock);
+				if( isEmpty() ) {
+					//The queue is empty. Sleep until it's not.
+					notified = false;	//But first tell the writers that they must wake us again!
+					std::unique_lock<std::mutex> l(lock);	//Only needed for the condition variable.
 
-				if ( Empty() && ! terminate ){
-					not_empty.wait(l, [=](){ return ( ! this->Empty() || terminate); });
+					if ( isEmpty() && ! terminate ){
+						not_empty.wait(l, [=](){ return ( ! this->isEmpty() || terminate); });
+					}
 				}
-
+				//We might just be woken up to be able to die...
 				if ( terminate ) {
+					terminated = true;
 					return nullptr;
 				}
+				assert( !isEmpty() );
 
-				auto activity = queue.front();
-				queue.pop_front();
-				return activity;
+				std::atomic_thread_fence( std::memory_order_acquire );
+				shared_ptr<Activity> result = buffer[indexMask & readIndex];
+				buffer[indexMask & readIndex];	//This has two effects: a) it ensures that the Activity can be destructed, and b) it ensures that `result` is actually read before `readIndex` is incremented.
+				std::atomic_thread_fence( std::memory_order_release );
+				readIndex++;
+				return result;
 			};
 
 			virtual void finalize() {
-				//printf("Queue:  Finalizing\n");
-				{
-				std::unique_lock<std::mutex> l(lock);
 				terminate = true;
-				}
-				
 				// this is important to wake up waiting pop/notifier
 				not_empty.notify_one();
+				while( !terminated ) ;	//Spin until we can safely destruct the object!
 			}
 	};
 
@@ -198,23 +182,14 @@ namespace monitoring {
 
 				// sleep(8); // for testing of overloading mode!
 
-				while( true ) {
-
-					shared_ptr<Activity> activity = queue->Pop();
-
-					if (! activity )
-					{
-					    break;
-					}
-					
+				while( shared_ptr<Activity> activity = queue->Pop() ) {
 					int lost = 0;
 
-					if( queue->overloaded && queue->Empty() ){
+					if( queue->overloaded && queue->isEmpty() ){
 					
 					    //	cout << "Overloading finished lost: " << queue->lost << endl;
 					    lost = queue->lost;
-					    // TODO small race condition here
-					    queue->lost = 0;
+					    queue->lost -= lost;	//not `= 0`, since we might have lost additional activities in the meantime.
 					    queue->overloaded = false;
 					}
 
@@ -251,7 +226,7 @@ namespace monitoring {
 	class ActivityMultiplexerAsync : public ActivityMultiplexer, public Dispatcher, public ComponentReportInterface {
 
 	private:
-			list<ActivityMultiplexerListener *> 		listeners;
+			vector<ActivityMultiplexerListener *> listeners;
 
 			ActivityMultiplexerQueue * queue = nullptr;
 			ActivityMultiplexerNotifier * notifier = nullptr;
@@ -298,15 +273,6 @@ namespace monitoring {
 				processed_activities++;
 				assert( activity != nullptr );
 
-				// quick sync dispatch
-				{
-					//unique_lock<mutex> onlyOne( sync_mutex );
-
-					for(auto l = listeners.begin(); l != listeners.end() ; l++){
-						(*l)->Notify(activity);
-					}
-				}
-				// add to async patch
 				if ( queue ) {
 					queue->Push(activity);
 				}
@@ -318,7 +284,7 @@ namespace monitoring {
 			 * @param	lost	lost activtiy count
 			 * @param	work	activtiy as void pointer to support abstract notifier
 			 */
-			void Dispatch(int lost, void * work) override { 	
+			void Dispatch(int lost, void * work) override {
 				shared_ptr<Activity> activity = *(shared_ptr<Activity>*) work;
 				assert( activity != nullptr );
 				//boost::shared_lock<boost::shared_mutex> lock( asyncQueueMutex );
@@ -335,13 +301,13 @@ namespace monitoring {
 			 * @param	listener	listener to be registered
 			 */
 			void registerListener( ActivityMultiplexerListener * listener ) override {
-				boost::unique_lock<boost::shared_mutex> lock( asyncQueueMutex );			
+				boost::unique_lock<boost::shared_mutex> lock( asyncQueueMutex );
 				listeners.push_back(listener);
 
 				// snipped conserved for later use
 				//boost::upgrade_lock<boost::shared_mutex> lock( asyncQueueMutex );
 				// if () {
-				// 		boost::upgrade_to_unique_lock<boost::shared_mutex> lock( asyncQueueMutex );
+				//     boost::upgrade_to_unique_lock<boost::shared_mutex> lock( asyncQueueMutex );
 				// }
 			}
 
@@ -353,7 +319,12 @@ namespace monitoring {
 			 */
 			void unregisterListener( ActivityMultiplexerListener * listener ) override {
 				boost::unique_lock<boost::shared_mutex> lock( asyncQueueMutex );
-				listeners.remove(listener);
+				for(size_t i = listeners.size(); i--; ) {
+					if(listeners[i] == listener) {
+						listeners[i] = listeners.back();
+						listeners.pop_back();
+					}
+				}
 			}
 
 			// Satisfy Component Requirements
