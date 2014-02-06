fe44ad27027d26b3cbe9d793aceea8034e2b2820
Alvaro Aguilera <alvaro.aguilera@tu-dresden.de>
2014-02-06 09:53:53 +0100

    Set version header to generic in HDF5 wrapper to get it compile.

--- tools/siox-skeleton-builder/layers/hdf5/hdf5-wrapper.h
+++ tools/siox-skeleton-builder/layers/hdf5/hdf5-wrapper.h
@@ -1,6 +1,8 @@
 #include "hdf5-helper.h"
 
-//@component "HDF5" implVersion "" unsigned major, minor, relnum; char implVersion[151]; int v_ret = H5get_libversion(& major, & minor, & relnum); if (v_ret >= 0) snprintf(implVersion, 150, "%d.%d", major, minor); else sprintf(implVersion, "0");
+//@component "HDF5" "Generic"
+
+//implVersion "" int major, minor, relnum; char implVersion[151]; int v_ret = H5get_libversion(& major, & minor, & relnum); if (v_ret >= 0) snprintf(implVersion, 150, "%d.%d", major, minor); else sprintf(implVersion, "0");
 
 //@register_attribute fileName "HDF5" "descriptor/filename" SIOX_STORAGE_STRING
 //@register_attribute fileHandle "HDF5" "descriptor/hid" SIOX_STORAGE_32_BIT_UINTEGER
