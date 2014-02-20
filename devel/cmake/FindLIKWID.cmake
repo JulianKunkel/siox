378c5ac4fa1e9d7300738cdf17578c18c50cab05
Alvaro Aguilera <alvaro.aguilera@tu-dresden.de>
2014-02-20 12:21:34 +0100

    Fixed assorted problems with cmake's LikWid.

--- devel/cmake/FindLIKWID.cmake
+++ devel/cmake/FindLIKWID.cmake
@@ -10,89 +10,34 @@
 #  For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 #
 
+set(LIKWID_INCLUDE_PATH_DESCRIPTION "top-level directory containing the LIKWID include directories.")
+set(LIKWID_INCLUDE_DIR_MESSAGE "Set the LIKWID_INCLUDE_DIR cmake cache entry to the ${LIKWID_INCLUDE_PATH_DESCRIPTION}")
+set(LIKWID_LIBRARY_PATH_DESCRIPTION "top-level directory containing the LIKWID libraries.")
+set(LIKWID_LIBRARY_DIR_MESSAGE "Set the LIKWID_LIBRARY_DIR cmake cache entry to the ${LIKWID_LIBRARY_PATH_DESCRIPTION}")
 
-IF (LIKWID_LIBRARIES AND LIKWID_INCLUDE_DIRS )
-	# in cache already
-	SET(LIKWID_FOUND TRUE)
-ELSE (LIKWID_LIBRARIES AND LIKWID_INCLUDE_DIRS )
-
-	INCLUDE(FindPkgConfig)
-
-	IF ( LIKWID_FIND_REQUIRED )
-		SET( _pkgconfig_REQUIRED "REQUIRED" )
-	ELSE ( LIKWID_FIND_REQUIRED )
-		SET( _pkgconfig_REQUIRED "" )
-	ENDIF ( LIKWID_FIND_REQUIRED )
-
-	IF ( PKG_CONFIG_FOUND )
-		IF ( LIKWID_FOUND )
-			SET ( LIKWID_CORE_FOUND TRUE )
-		ELSE ( LIKWID_FOUND )
-			SET ( LIKWID_CORE_FOUND FALSE )
-		ENDIF ( LIKWID_FOUND )
-	ENDIF ( PKG_CONFIG_FOUND )
-
-	# Look for LIKWID include dir and libraries
-	IF ( NOT LIKWID_FOUND )
-		FIND_PATH(
-			LIKWID_include_DIR
-			NAMES
-			likwid.h
-			perfmon.h
-			PATHS
-			/opt/lib/
-			/opt/local/lib
-			/sw/lib/
-			/usr/lib64
-			/usr/lib
-			/usr/local/include
-			${CMAKE_LIBRARY_PATH}
-			PATH_SUFFIXES
-			likwid/include
-		)
-
-		FIND_LIBRARY(
-			LIKWID_link_DIR
-			NAMES
-			likwid
-			PATHS
-			/opt/local/lib
-			/sw/lib
-			/usr/lib
-			/usr/local/lib
-		)
-
-		IF ( LIKWID_include_DIR AND LIKWID_link_DIR )
-			SET ( LIKWID_FOUND TRUE )
-		ENDIF ( LIKWID_include_DIR AND LIKWID_link_DIR )
+find_path(LIKWID_INCLUDE_DIRS 
+        NAME
+                likwid
+        PATHS
+                ${LIKWID_INCLUDE_DIR}
+        DOC
+                "Directory for LIKWID headers"
+)
 
 
-		IF ( LIKWID_FOUND )
-			SET ( LIKWID_INCLUDE_DIRS ${LIKWID_include_DIR} ${LIKWID_include_DIR}/likwid )
-			SET ( LIKWID_LIBRARIES -L${LIKWID_link_DIR} -llikwid)
-			SET ( LIKWID_CORE_FOUND TRUE )
-		ELSE ( LIKWID_FOUND )
-			SET ( LIKWID_CORE_FOUND FALSE )
-		ENDIF ( LIKWID_FOUND )
+find_library(LIKWID_LIBRARIES 
+        NAMES 
+                likwid
+        PATHS
+                ${LIKWID_LIBRARY_DIR}
+        DOC
+                "Directory for LIKWID binary"
+)
 
-	ENDIF ( NOT LIKWID_FOUND  )
+include(FindPackageHandleStandardArgs)
 
-	IF (LIKWID_CORE_FOUND AND LIKWID_INCLUDE_DIRS AND LIKWID_LIBRARIES)
-		SET (LIKWID_FOUND TRUE)
-	ENDIF (LIKWID_CORE_FOUND AND LIKWID_INCLUDE_DIRS AND LIKWID_LIBRARIES)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(LIKWID DEFAULT_MSG LIKWID_INCLUDE_DIRS LIKWID_LIBRARIES)
 
-	IF (LIKWID_FOUND)
-		IF (NOT LIKWID_FIND_QUIETLY)
-			MESSAGE (STATUS "Found LIKWID: ${LIKWID_LIBRARIES} ${LIKWID_INCLUDE_DIRS}")
-		ENDIF (NOT LIKWID_FIND_QUIETLY)
-	ELSE (LIKWID_FOUND)
-		IF (LIKWID_FIND_REQUIRED)
-			MESSAGE (SEND_ERROR "Could not find LIKWID")
-		ENDIF (LIKWID_FIND_REQUIRED)
-	ENDIF (LIKWID_FOUND)
+mark_as_advanced(LIKWID_INCLUDE_DIRS LIKWID_LIBRARIES)
 
-	# show the LIKWID_INCLUDE_DIRS and LIKWID_LIBRARIES variables only in the advanced view
-	MARK_AS_ADVANCED(LIKWID_INCLUDE_DIRS LIKWID_LIBRARIES)
-
-ENDIF (LIKWID_LIBRARIES AND LIKWID_INCLUDE_DIRS)
 
