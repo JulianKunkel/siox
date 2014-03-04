a9f455061f92d70528a29acf89caa73577bf9ff3
Alvaro Aguilera <alvaro.aguilera@tu-dresden.de>
2014-03-04 10:21:02 +0100

    Cosmetic changes.

--- tools/web/www/header.php
+++ tools/web/www/header.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
-<title><?=$site_title?></title>
-<meta charset="utf-8" />
-<meta name="description" content="<?=$site_description?>" />
-<link rel="icon" type="image/ico" href="favicon.ico" />
-<link rel="stylesheet" type="text/css" href="siox.css" media="all">
+	<title><?=$site_title?></title>
+	<meta charset="utf-8" />
+	<meta name="description" content="<?=$site_description?>" />
+	<link rel="icon" type="image/png" href="/siox/favicon.png" />
+	<link rel="stylesheet" type="text/css" href="siox.css" media="all">
 </head>
 <body>
