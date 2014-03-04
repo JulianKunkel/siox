1047b1691a5e48a16b8b7c00a4cb705c83dac177
Alvaro Aguilera <alvaro.aguilera@tu-dresden.de>
2014-03-04 14:53:26 +0100

    Bug fixed.

--- tools/web/www/include/SIOX.php
+++ tools/web/www/include/SIOX.php
@@ -11,31 +11,8 @@ static function purge()
 
 	if (!$stmt->execute()) {
 		print_r($dbcon->errorInfo());
-		die("Error purging SIOX database,");
+		die("Error purging SIOX database.");
 	}
-
-}
-
-static function get_program_list()
-{
-	global $dbcon;
-
-	$sql = "SELECT * FROM get_program_list()";
-	$stmt = $dbcon->prepare($sql);
-
-	if (!$stmt->execute)) {
-		print_r($dbcon->errorInfo());
-		die("Error getting program list.");
-	}
-
-	$progs = array();
-
-	while ($row = $stmt->fetch(PDO::FETCH_OBJ))
-		$progs[] = $row;
-
-	return $progs;
-}
-
 
 }
 
