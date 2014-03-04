c0f2bf17646538542efba2bf3421121ec3ecc7ee
Alvaro Aguilera <alvaro.aguilera@tu-dresden.de>
2014-03-04 14:26:13 +0100

    First steps to program overview.

---
+++ tools/web/www/include/Program.php
@@ -0,0 +1,76 @@
+<?php
+
+class Program {
+
+static function get_count()
+{
+	global $dbcon;
+
+	$sql = "SELECT count(*) FROM get_program_list()";
+	$stmt = $dbcon->prepare($sql);
+
+	if (!$stmt->execute()) {
+		print_r($dbcon->errorInfo());
+		die("Error getting program count.");
+	}
+
+	$row = $stmt->fetch(PDO::FETCH_OBJ);
+	return $row->count;
+
+}
+
+
+static function get_attributes($procid)
+{
+	global $dbcon;
+
+	$sql = "SELECT * FROM topology.get_attributes_by_proc_number(:pid)";
+
+	$stmt = $dbcon->prepare($sql);
+	$stmt->bindParam(':pid', $procid);
+
+	if (!$stmt->execute()) {
+		print_r($dbcon->errorInfo());
+		die("Error getting program attributes.");
+	}
+
+	$attrs = array();
+
+	while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
+		$attrs[$row->key] = $row->val;
+	}
+
+	return $attrs;
+}
+
+
+static function get_list($page = 1, $page_size = 200)
+{
+	global $dbcon;
+
+	$sql = "SELECT * FROM get_program_list() LIMIT :page_size OFFSET :page_offset";
+	
+	$stmt = $dbcon->prepare($sql);
+	$stmt->bindParam(':page_size', $page_size);
+	$stmt->bindValue(':page_offset', $page_size*($page-1));
+
+	if (!$stmt->execute()) {
+		print_r($dbcon->errorInfo());
+		die("Error getting program list.");
+	}
+
+	$list = array();
+
+	while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
+		$row->attributes = self::get_attributes($row->childobjectid);
+		$list[] = $row;
+	}
+
+	return $list;
+
+}
+
+
+}
+
+?>
--- tools/web/www/include/SIOX.php
+++ tools/web/www/include/SIOX.php
@@ -16,6 +16,29 @@ static function purge()
 
 }
 
+static function get_program_list()
+{
+	global $dbcon;
+
+	$sql = "SELECT * FROM get_program_list()";
+	$stmt = $dbcon->prepare($sql);
+
+	if (!$stmt->execute)) {
+		print_r($dbcon->errorInfo());
+		die("Error getting program list.");
+	}
+
+	$progs = array();
+
+	while ($row = $stmt->fetch(PDO::FETCH_OBJ))
+		$progs[] = $row;
+
+	return $progs;
+}
+
+
+}
+
 }
 
 ?>
