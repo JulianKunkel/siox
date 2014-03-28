b4ddb2a52b266165d67225dd007f71d0a4ff1642
Michaela Zimmer <Michaela.Zimmer@Informatik.Uni-Hamburg.de>
2014-03-28 22:03:08 +0100

    Fixed a bug in the POSIX wrapper.

--- tools/siox-skeleton-builder/layers/posix/posix-low-level-io.h
+++ tools/siox-skeleton-builder/layers/posix/posix-low-level-io.h
@@ -644,15 +644,15 @@ Otherwise, a non-zero value is returned; This may be due to an invalid mode para
 int setvbuf( FILE * stream, char * buffer, int mode, size_t size );
 
 /*
-Unlink deletes a name from the file system and possibly but not in all cases the file it refers to 
+Unlink deletes a name from the file system and possibly but not in all cases the file it refers to
 It only deletes the file if there are no other processes that have the file open.
 On success zero is returned. On error -1 is returned
 */
 //@guard
 //@errorErrno ''ret<0''
 //@activity
-//@splice_before SET_PATHNAME(pathname)
-int unlink(const char *pathname); 
+//@splice_before SET_FILENAME(pathname);
+int unlink(const char *pathname);
 
 /*
 On success, the total number of characters written is returned.
