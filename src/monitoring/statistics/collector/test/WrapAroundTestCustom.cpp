1b1028410b757bfccdc4a53892bf97b1bae2736a
Nathanael Hübbe <nathanael.huebbe@informatik.uni-hamburg.de>
2014-05-20 17:05:52 +0200

    added test case for a crazy custom wraparound

---
+++ src/monitoring/statistics/collector/test/WrapAroundTestCustom.cpp
@@ -0,0 +1,129 @@
+/**
+ * @file WrapAroundTest.cpp
+ *
+ * @author Nathanael Hübbe
+ * @date   2014
+ */
+
+#include <iostream>
+
+#include <core/module/ModuleLoader.hpp>
+
+#include <monitoring/statistics/collector/StatisticsProviderPlugin.hpp>
+#include <monitoring/statistics/collector/StatisticsCollector.hpp>
+
+#include <monitoring/statistics/Statistic.hpp>
+#include <monitoring/statistics/multiplexer/StatisticsMultiplexer.hpp>
+#include <monitoring/topology/Topology.hpp>
+#include <monitoring/ontology/Ontology.hpp>
+#include <knowledge/activity_plugin/ActivityPluginDereferencing.hpp>
+#include <knowledge/activity_plugin/DereferencingFacadeOptions.hpp>
+
+#include "../../../../monitoring/ontology/modules/TopologyOntology/TopologyOntologyOptions.hpp"
+#include "../../collector/ThreadedStatisticsOptions.hpp"
+
+
+using namespace core;
+using namespace monitoring;
+
+const uint64_t kMin = 23;
+const uint64_t kMax = 48;
+const uint64_t kStart = 29;
+const uint64_t kIncrement = 10;
+
+class ProviderDummy : public StatisticsProviderPlugin {
+	public:
+		ProviderDummy() {
+			metric = (uint64_t)kStart;
+			cout << "ProviderDummy: Setting metric to " << metric << "\n";
+		}
+		virtual void nextTimestep() throw() {
+			metric += kIncrement;
+			if(metric > kMax) metric += -(kMax + 1 - kMin);
+			cout << "ProviderDummy: Setting metric to " << metric << "\n";
+		}
+		virtual vector<StatisticsProviderDatatypes> availableMetrics() throw() {
+			vector<StatisticsProviderDatatypes> result;
+			result.push_back(StatisticsProviderDatatypes("foo", "@localhost", metric, INCREMENTAL, "bar", "baz", kMax, kMin));
+			return result;
+		}
+	private:
+		StatisticsValue metric;
+};
+
+class MuxDummy : public StatisticsMultiplexer {
+	public:
+		virtual ComponentOptions* AvailableOptions() {
+			ComponentOptions* options = new ComponentOptions();
+			return options;
+		}
+		virtual void init() {}
+		virtual void notifyAvailableStatisticsChange( const std::vector<std::shared_ptr<Statistic> > & statistics, bool addedStatistics, bool removedStatistics ) throw() override {
+			assert(!myStatistic);
+			if(statistics.size()) myStatistic = statistics[0];
+		}
+		virtual void newDataAvailable() throw() override {
+			if(myStatistic) {
+				cout << "MuxDummy: myStatistic == " << myStatistic->curValue << "\n";
+				if(gotFirstValue) {
+					assert(myStatistic->curValue == kIncrement);
+				}
+				gotFirstValue = true;
+			}
+		}
+		virtual void registerListener( StatisticsMultiplexerListener* listener ) throw() override {}
+		virtual void unregisterListener( StatisticsMultiplexerListener* listener ) throw() override {}
+	private:
+		std::shared_ptr<Statistic> myStatistic;
+		bool gotFirstValue = false;
+};
+
+int main( int argc, char const * argv[] ) throw() {
+	#define makeModule(Type, PointerName, ModuleName, InterfaceName) \
+		Type* PointerName = module_create_instance<Type>( "", ModuleName, InterfaceName)
+	makeModule( Topology, topology, "siox-monitoring-RamTopology", MONITORING_TOPOLOGY_INTERFACE );
+	makeModule( ActivityPluginDereferencing, facade, "siox-knowledge-DereferencingFacade", ACTIVITY_DEREFERENCING_FACADE_INTERFACE );
+	makeModule( Ontology, ontology, "siox-monitoring-TopologyOntology", ONTOLOGY_INTERFACE );
+	makeModule( StatisticsCollector, collector, "siox-monitoring-ThreadedStatisticsCollector", STATISTICS_COLLECTOR_INTERFACE );
+	ProviderDummy* provider = new ProviderDummy();
+	MuxDummy* mux = new MuxDummy();
+
+	#define fetchOptions(Type, ComponentVariableName) \
+		Type* ComponentVariableName##Options = &ComponentVariableName->getOptions<Type>();
+	fetchOptions( ComponentOptions, topology );
+	fetchOptions( DereferencingFacadeOptions, facade );
+	fetchOptions( TopologyOntologyOptions, ontology );
+	fetchOptions( ThreadedStatisticsOptions, collector );
+	fetchOptions( StatisticsProviderPluginOptions, provider );
+	fetchOptions( ComponentOptions, mux );
+
+	(void)topologyOptions;	//avoid warning about unused variable
+	facadeOptions->topology.componentPointer = topology;
+	ontologyOptions->topology.componentPointer = topology;
+	collectorOptions->topology = topology;
+	collectorOptions->ontology.componentPointer = ontology;
+	collectorOptions->smux.componentPointer = mux;
+	providerOptions->statisticsCollector = collector;
+	(void)muxOptions;	//avoid warning about unused variable
+
+	topology->init();
+	ontology->init();
+	facade->init();
+	mux->init();
+	collector->init();
+	collector->start();
+	provider->init();
+
+	cerr << "sleeping\n";
+	sleep( 1 );
+	cerr << "waking up\n";
+
+	collector->stop();
+	delete collector;
+	delete ontology;
+	delete topology;
+
+	cerr << argv[0] << ": OK" << endl;
+	return 0;
+}
+
