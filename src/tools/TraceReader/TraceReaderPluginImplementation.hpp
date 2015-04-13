be598f1c60cd84b9bbfff4fa7e55043ae59c0b3f
eugen.betke@gmail.com <eugen.betke@gmail.com>
2015-04-13 10:45:04 +0200

    Remove old plugins (Print, AccessInfoPlotter), remove TraceReader test

--- src/tools/TraceReader/TraceReaderPluginImplementation.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef TOOLS_TRACE_READER_PLUGIN_IMPL_
-#define TOOLS_TRACE_READER_PLUGIN_IMPL_
-#include <tools/TraceReader/TraceReaderPlugin.hpp>
-
-#define TOOLS_TRACE_READER_INSTANCIATOR_NAME MODULE_INSTANCIATOR_NAME(tools_traceReader_plugin)
-
-#endif
