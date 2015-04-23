8a2990ef8ff518d30426bc5499fd7edaf38b2cb9
Julian M. Kunkel <juliankunkel@googlemail.com>
2015-04-23 20:00:16 +0200

    POSIX: convert machine-specific error to a portable number.

--- tools/siox-skeleton-builder/layers/posix/posix-low-level-io-helper.h
+++ tools/siox-skeleton-builder/layers/posix/posix-low-level-io-helper.h
@@ -1,5 +1,7 @@
 #include "siox-datatypes-helper-posix.h"
 
+#include <errno.h>
+
 #include <sys/types.h>
 #include <unistd.h>
 #include <glib.h>
@@ -38,17 +40,17 @@ static inline unsigned translateFILEFlagsToSIOX( const char * str )
 
 	for(const char * c = str; *c != '\0' ; c++ ){
 		switch(*c){
-			case 'a':
+			case 'a' :
 				flags |= SIOX_LOW_LEVEL_O_APPEND | SIOX_LOW_LEVEL_O_CREAT;
 				break;
-			case 'r':
+			case 'r' :
 				flags |= SIOX_LOW_LEVEL_O_RDONLY;
 				break;
-			case 'w':
+			case 'w' :
 				flags |= SIOX_LOW_LEVEL_O_WRONLY | SIOX_LOW_LEVEL_O_CREAT | SIOX_LOW_LEVEL_O_TRUNC;
 				break;
 			case '+':
-				flags |= SIOX_LOW_LEVEL_O_RDWR;
+  				flags |= SIOX_LOW_LEVEL_O_RDWR;
 				break;
 		}
 	}
@@ -65,3 +67,419 @@ static inline unsigned translateFILEFlagsToSIOX( const char * str )
 static inline unsigned prepareIovec( const struct iovec * iov, int iovcnt ) {
 	return 0;
 }
+
+
+
+static inline int translateErrno(int err){
+	switch(err){
+		case (0):{
+			return SIOX_ESUCCESS;
+		}
+		case (EPERM) : {
+			return SIOX_EPERM;
+		}
+		case (ENOENT) : {
+			return SIOX_ENOENT;
+		}
+		case (ESRCH) : {
+			return SIOX_ESRCH;
+		}
+		case (EINTR) : {
+			return SIOX_EINTR;
+		}
+		case (EIO) : {
+			return SIOX_EIO;
+		}
+		case (ENXIO) : {
+			return SIOX_ENXIO;
+		}
+		case (E2BIG) : {
+			return SIOX_E2BIG;
+		}
+		case (ENOEXEC) : {
+			return SIOX_ENOEXEC;
+		}
+		case (EBADF) : {
+			return SIOX_EBADF;
+		}
+		case (ECHILD) : {
+			return SIOX_ECHILD;
+		}
+		case (EAGAIN) : {
+			return SIOX_EAGAIN;
+		}
+		case (ENOMEM) : {
+			return SIOX_ENOMEM;
+		}
+		case (EACCES) : {
+			return SIOX_EACCES;
+		}
+		case (EFAULT) : {
+			return SIOX_EFAULT;
+		}
+		case (ENOTBLK) : {
+			return SIOX_ENOTBLK;
+		}
+		case (EBUSY) : {
+			return SIOX_EBUSY;
+		}
+		case (EEXIST) : {
+			return SIOX_EEXIST;
+		}
+		case (EXDEV) : {
+			return SIOX_EXDEV;
+		}
+		case (ENODEV) : {
+			return SIOX_ENODEV;
+		}
+		case (ENOTDIR) : {
+			return SIOX_ENOTDIR;
+		}
+		case (EISDIR) : {
+			return SIOX_EISDIR;
+		}
+		case (EINVAL) : {
+			return SIOX_EINVAL;
+		}
+		case (ENFILE) : {
+			return SIOX_ENFILE;
+		}
+		case (EMFILE) : {
+			return SIOX_EMFILE;
+		}
+		case (ENOTTY) : {
+			return SIOX_ENOTTY;
+		}
+		case (ETXTBSY) : {
+			return SIOX_ETXTBSY;
+		}
+		case (EFBIG) : {
+			return SIOX_EFBIG;
+		}
+		case (ENOSPC) : {
+			return SIOX_ENOSPC;
+		}
+		case (ESPIPE) : {
+			return SIOX_ESPIPE;
+		}
+		case (EROFS) : {
+			return SIOX_EROFS;
+		}
+		case (EMLINK) : {
+			return SIOX_EMLINK;
+		}
+		case (EPIPE) : {
+			return SIOX_EPIPE;
+		}
+		case (EDOM) : {
+			return SIOX_EDOM;
+		}
+		case (ERANGE) : {
+			return SIOX_ERANGE;
+		}
+		case (EDEADLK) : {
+			return SIOX_EDEADLK;
+		}
+		case (ENAMETOOLONG) : {
+			return SIOX_ENAMETOOLONG;
+		}
+		case (ENOLCK) : {
+			return SIOX_ENOLCK;
+		}
+		case (ENOSYS) : {
+			return SIOX_ENOSYS;
+		}
+		case (ENOTEMPTY) : {
+			return SIOX_ENOTEMPTY;
+		}
+		case (ELOOP) : {
+			return SIOX_ELOOP;
+		}
+		#if (EWOULDBLOCK != EAGAIN)
+		case (EWOULDBLOCK) : {
+			return SIOX_EWOULDBLOCK;
+		}
+		#endif 
+		case (ENOMSG) : {
+			return SIOX_ENOMSG;
+		}
+		case (EIDRM) : {
+			return SIOX_EIDRM;
+		}
+		case (ECHRNG) : {
+			return SIOX_ECHRNG;
+		}
+		case (EL2NSYNC) : {
+			return SIOX_EL2NSYNC;
+		}
+		case (EL3HLT) : {
+			return SIOX_EL3HLT;
+		}
+		case (EL3RST) : {
+			return SIOX_EL3RST;
+		}
+		case (ELNRNG) : {
+			return SIOX_ELNRNG;
+		}
+		case (EUNATCH) : {
+			return SIOX_EUNATCH;
+		}
+		case (ENOCSI) : {
+			return SIOX_ENOCSI;
+		}
+		case (EL2HLT) : {
+			return SIOX_EL2HLT;
+		}
+		case (EBADE) : {
+			return SIOX_EBADE;
+		}
+		case (EBADR) : {
+			return SIOX_EBADR;
+		}
+		case (EXFULL) : {
+			return SIOX_EXFULL;
+		}
+		case (ENOANO) : {
+			return SIOX_ENOANO;
+		}
+		case (EBADRQC) : {
+			return SIOX_EBADRQC;
+		}
+		case (EBADSLT) : {
+			return SIOX_EBADSLT;
+		}
+		#if (EDEADLOCK != EDEADLK)
+		case (EDEADLOCK) : {
+			return SIOX_EDEADLOCK;
+		}
+		#endif
+		case (EBFONT) : {
+			return SIOX_EBFONT;
+		}
+		case (ENOSTR) : {
+			return SIOX_ENOSTR;
+		}
+		case (ENODATA) : {
+			return SIOX_ENODATA;
+		}
+		case (ETIME) : {
+			return SIOX_ETIME;
+		}
+		case (ENOSR) : {
+			return SIOX_ENOSR;
+		}
+		case (ENONET) : {
+			return SIOX_ENONET;
+		}
+		case (ENOPKG) : {
+			return SIOX_ENOPKG;
+		}
+		case (EREMOTE) : {
+			return SIOX_EREMOTE;
+		}
+		case (ENOLINK) : {
+			return SIOX_ENOLINK;
+		}
+		case (EADV) : {
+			return SIOX_EADV;
+		}
+		case (ESRMNT) : {
+			return SIOX_ESRMNT;
+		}
+		case (ECOMM) : {
+			return SIOX_ECOMM;
+		}
+		case (EPROTO) : {
+			return SIOX_EPROTO;
+		}
+		case (EMULTIHOP) : {
+			return SIOX_EMULTIHOP;
+		}
+		case (EDOTDOT) : {
+			return SIOX_EDOTDOT;
+		}
+		case (EBADMSG) : {
+			return SIOX_EBADMSG;
+		}
+		case (EOVERFLOW) : {
+			return SIOX_EOVERFLOW;
+		}
+		case (ENOTUNIQ) : {
+			return SIOX_ENOTUNIQ;
+		}
+		case (EBADFD) : {
+			return SIOX_EBADFD;
+		}
+		case (EREMCHG) : {
+			return SIOX_EREMCHG;
+		}
+		case (ELIBACC) : {
+			return SIOX_ELIBACC;
+		}
+		case (ELIBBAD) : {
+			return SIOX_ELIBBAD;
+		}
+		case (ELIBSCN) : {
+			return SIOX_ELIBSCN;
+		}
+		case (ELIBMAX) : {
+			return SIOX_ELIBMAX;
+		}
+		case (ELIBEXEC) : {
+			return SIOX_ELIBEXEC;
+		}
+		case (EILSEQ) : {
+			return SIOX_EILSEQ;
+		}
+		case (ERESTART) : {
+			return SIOX_ERESTART;
+		}
+		case (ESTRPIPE) : {
+			return SIOX_ESTRPIPE;
+		}
+		case (EUSERS) : {
+			return SIOX_EUSERS;
+		}
+		case (ENOTSOCK) : {
+			return SIOX_ENOTSOCK;
+		}
+		case (EDESTADDRREQ) : {
+			return SIOX_EDESTADDRREQ;
+		}
+		case (EMSGSIZE) : {
+			return SIOX_EMSGSIZE;
+		}
+		case (EPROTOTYPE) : {
+			return SIOX_EPROTOTYPE;
+		}
+		case (ENOPROTOOPT) : {
+			return SIOX_ENOPROTOOPT;
+		}
+		case (EPROTONOSUPPORT) : {
+			return SIOX_EPROTONOSUPPORT;
+		}
+		case (ESOCKTNOSUPPORT) : {
+			return SIOX_ESOCKTNOSUPPORT;
+		}
+		case (EOPNOTSUPP) : {
+			return SIOX_EOPNOTSUPP;
+		}
+		case (EPFNOSUPPORT) : {
+			return SIOX_EPFNOSUPPORT;
+		}
+		case (EAFNOSUPPORT) : {
+			return SIOX_EAFNOSUPPORT;
+		}
+		case (EADDRINUSE) : {
+			return SIOX_EADDRINUSE;
+		}
+		case (EADDRNOTAVAIL) : {
+			return SIOX_EADDRNOTAVAIL;
+		}
+		case (ENETDOWN) : {
+			return SIOX_ENETDOWN;
+		}
+		case (ENETUNREACH) : {
+			return SIOX_ENETUNREACH;
+		}
+		case (ENETRESET) : {
+			return SIOX_ENETRESET;
+		}
+		case (ECONNABORTED) : {
+			return SIOX_ECONNABORTED;
+		}
+		case (ECONNRESET) : {
+			return SIOX_ECONNRESET;
+		}
+		case (ENOBUFS) : {
+			return SIOX_ENOBUFS;
+		}
+		case (EISCONN) : {
+			return SIOX_EISCONN;
+		}
+		case (ENOTCONN) : {
+			return SIOX_ENOTCONN;
+		}
+		case (ESHUTDOWN) : {
+			return SIOX_ESHUTDOWN;
+		}
+		case (ETOOMANYREFS) : {
+			return SIOX_ETOOMANYREFS;
+		}
+		case (ETIMEDOUT) : {
+			return SIOX_ETIMEDOUT;
+		}
+		case (ECONNREFUSED) : {
+			return SIOX_ECONNREFUSED;
+		}
+		case (EHOSTDOWN) : {
+			return SIOX_EHOSTDOWN;
+		}
+		case (EHOSTUNREACH) : {
+			return SIOX_EHOSTUNREACH;
+		}
+		case (EALREADY) : {
+			return SIOX_EALREADY;
+		}
+		case (EINPROGRESS) : {
+			return SIOX_EINPROGRESS;
+		}
+		case (ESTALE) : {
+			return SIOX_ESTALE;
+		}
+		case (EUCLEAN) : {
+			return SIOX_EUCLEAN;
+		}
+		case (ENOTNAM) : {
+			return SIOX_ENOTNAM;
+		}
+		case (ENAVAIL) : {
+			return SIOX_ENAVAIL;
+		}
+		case (EISNAM) : {
+			return SIOX_EISNAM;
+		}
+		case (EREMOTEIO) : {
+			return SIOX_EREMOTEIO;
+		}
+		case (EDQUOT) : {
+			return SIOX_EDQUOT;
+		}
+		case (ENOMEDIUM) : {
+			return SIOX_ENOMEDIUM;
+		}
+		case (EMEDIUMTYPE) : {
+			return SIOX_EMEDIUMTYPE;
+		}
+		case (ECANCELED) : {
+			return SIOX_ECANCELED;
+		}
+		case (ENOKEY) : {
+			return SIOX_ENOKEY;
+		}
+		case (EKEYEXPIRED) : {
+			return SIOX_EKEYEXPIRED;
+		}
+		case (EKEYREVOKED) : {
+			return SIOX_EKEYREVOKED;
+		}
+		case (EKEYREJECTED) : {
+			return SIOX_EKEYREJECTED;
+		}
+		case (EOWNERDEAD) : {
+			return SIOX_EOWNERDEAD;
+		}
+		case (ENOTRECOVERABLE) : {
+			return SIOX_ENOTRECOVERABLE;
+		}
+		case (ERFKILL) : {
+			return SIOX_ERFKILL;
+		}
+		case (EHWPOISON) : {
+			return SIOX_EHWPOISON;
+		}
+	default:
+		return SIOX_ERR_UNKNOWN;
+	}
+}
+
