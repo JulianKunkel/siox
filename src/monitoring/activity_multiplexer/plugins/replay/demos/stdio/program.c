424311b0628150f73f07264bfccf209499504623
Jakob Luettgau <jakob@whatz.eu>
2015-07-07 14:20:51 +0200

    stdio replay case

---
+++ src/monitoring/activity_multiplexer/plugins/replay/demos/stdio/program.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	const char * file = "test.tmp";
+
+	// write to non existing file
+	int fd = open(	file,
+					O_WRONLY | O_CREAT,
+					S_IWUSR | S_IRUSR | S_IWGRP | S_IRGRP | S_IROTH
+				);
+
+	// everything ok? write and close
+	if ( fd != -1 )
+	{
+		write(fd, "abc", 3);
+		write(fd, "def", 3);
+		close(fd);
+	}
+
+	//printf("%s written.\n", file);
+
+	return 0;
+}
