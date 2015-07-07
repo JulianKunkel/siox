f7d71f29e29b32f5c674ab6205741dd4fc6aca4d
Jakob Luettgau <jakob@whatz.eu>
2015-07-07 14:21:17 +0200

    streaming io demo case

---
+++ src/monitoring/activity_multiplexer/plugins/replay/demos/stream/program.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	const char * file = "test.tmp";
+	FILE * stream;
+	const char * buf = "abcdef";
+
+	// write to non existing file
+	stream = fopen(file, "w");
+
+	// everything ok? write and close
+	if ( !stream ) {
+		perror("fopen");
+	}
+
+	if ( !fwrite(buf, sizeof(char), 3, stream) ) { perror("fwrite"); }
+	if ( !fwrite(buf+3, sizeof(char), 3, stream) ) { perror("fwrite"); }
+
+	//fwrite(buf+3, sizeof(char), 3, stream);
+	if (fclose(stream)) {
+		perror("fclose");		
+	}
+
+	//printf("%s written.\n", file);
+
+	return 0;
+}
