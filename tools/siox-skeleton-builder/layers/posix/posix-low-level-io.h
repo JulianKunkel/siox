0211677407e92272bc0bfcc5710df42334a4d75b
Julian Kunkel <juliankunkel@googlemail.com>
2016-09-21 10:02:56 +0200

    Lseek64 fix.

--- tools/siox-skeleton-builder/layers/posix/posix-low-level-io.h
+++ tools/siox-skeleton-builder/layers/posix/posix-low-level-io.h
@@ -283,6 +283,14 @@ int fdatasync( int fd );
 //@activity_attribute_late filePosition ret
 off_t lseek(int fd, off_t offset, int whence);
 
+//@guard
+//@errorErrno ''ret == (off_t) -1''
+//@activity
+//@activity_attribute fileHandle fd
+//@activity_link_int fd
+//@activity_attribute_late filePosition ret
+//@activity Name=lseek
+off_t lseek64(int fd, off64_t offset, int whence);
 
 
 /* Hints to the system, this might be used for optimizations by SIOX in the future */
