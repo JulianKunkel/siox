5b9dedd732d6bc530a8920371c28a1d822b67437
eugen.betke <betke@dkrz.de>
2016-10-24 19:54:06 +0200

    Small improvements

--- src/monitoring/activity_multiplexer/plugins/file_access_info/test/usage-example.sh
+++ src/monitoring/activity_multiplexer/plugins/file_access_info/test/usage-example.sh
@@ -8,32 +8,33 @@
 # CHANGE THIS PATH #
 INSTALL="/work/ku0598/k202107/git/siox/siox-access_info_plotter/install/"
 
-
 # Convinience variables are not necessary, when siox is properly installed.
 INST="$INSTALL/bin/siox-inst"
 TRACEREADER="$INSTALL/bin/siox-trace-reader"
 LD_LIBRARY_PATH="$INSTALL/lib64:$LD_LIBRARY_PATH"
-TESTPROG="w"
+#TESTPROG="w"
+TESTPROG="../../../../../../tools/siox-skeleton-builder/layers/posix/test/mmap_rw"
 
 # Clean up
 rm activities.dat*
 
+
 ### STEP 1 ###
 # siox-inst creates for each process an activity file. Depending on application you can get a number of files.
+set -x
 $INST posix $TESTPROG
+set +x
 
 
 ### STEP 2 ###
 # siox-trace-reader processes one single file at a time, therefore we need a list of files.
 FILES=($(find -regextype posix-egrep  -regex './activities.dat[[:digit:]]*'))
 
-
 for FILE in ${FILES[@]}
 do
-	# All supported options you can list with the following command:
-	# siox-trace-reader --help
-
 	# Apply our plugin to each file in the list.
+	set -x
 	${TRACEREADER} --FileAIStreamOptions.filename $FILE --FileAccessInfoPluginOptions.output $FILE.txt
+	set +x
 done
 
